8fdec087c959269119839482b15b356ca22da947
HelviraG <pasiphae.code@gmail.com>
2023-12-14 15:31:04 +0100

    fix(cta): broken link

--- src/App/Pages/Home/HomeCTA.tsx
+++ src/App/Pages/Home/HomeCTA.tsx
@@ -10,10 +10,10 @@ export const HomeCTA = () => {
 
     return (
         <HomeCTABox>
-            <HomeCTAButton startIcon={<PlayCircleOutlineRoundedIcon />} href={Routes.CONFS}>
+            <HomeCTAButton startIcon={<PlayCircleOutlineRoundedIcon />} href={`/#${Routes.CONFS}`}>
                 {t('app.home.welcome.actions.confs')}
             </HomeCTAButton>
-            <HomeCTAButton startIcon={<ForwardToInboxRoundedIcon />} href={Routes.CONTACT}>
+            <HomeCTAButton startIcon={<ForwardToInboxRoundedIcon />} href={`/#${Routes.CONTACT}`}>
                 {t('app.home.welcome.actions.contact')}
             </HomeCTAButton>
         </HomeCTABox>
