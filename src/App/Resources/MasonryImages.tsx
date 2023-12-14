5305e6e7d71ea27ef9f3fac7816611d0b922cd19
HelviraG <pasiphae.code@gmail.com>
2023-12-14 07:46:38 +0100

    feat(Masonry): rework image bands

--- src/App/Resources/MasonryImages.tsx
+++ src/App/Resources/MasonryImages.tsx
@@ -1,4 +1,4 @@
-export const MasonryImages = () => {
+export const MasonryImagesTop = () => {
     return [
         {
             img: 'https://i.ibb.co/d7FJhKm/20230531-200845.jpg',
@@ -15,6 +15,27 @@ export const MasonryImages = () => {
         {
             img: 'https://i.ibb.co/5MsJjvk/DSC08554.jpg',
             title: 'DevFest Lille 2023',
+        }
+    ];
+};
+
+export const MasonryImagesBottom = () => {
+    return [
+        {
+            img: 'https://i.ibb.co/2cntgRT/square-media-img-2.png',
+            title: 'Portrait Square Media',
+        },
+        {
+            img: 'https://i.ibb.co/6gZkH9m/1668788757970.jpg',
+            title: 'Codeurs en Seine 2022',
+        },
+        {
+            img: 'https://i.ibb.co/17Dg9Qn/bilan-8.jpg',
+            title: 'Live Welovedevs',
+        },
+        {
+            img: 'https://i.ibb.co/dbtLVGK/IMG-20230922-WA0007.jpg',
+            title: 'Api Platform Conference 2023',
         }
     ];
 };
