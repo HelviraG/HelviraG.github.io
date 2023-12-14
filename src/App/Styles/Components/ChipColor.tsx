196d0da8583280a7a451f55b650fd35d6d585490
HelviraG <pasiphae.code@gmail.com>
2023-12-14 07:39:43 +0100

    feat(app): add missing chip colors

--- src/App/Styles/Components/ChipColor.tsx
+++ src/App/Styles/Components/ChipColor.tsx
@@ -1,8 +1,29 @@
+import { useTheme } from '@mui/material';
 import { Tags } from '../../Resources/Enums/Tags';
 
 export const ChipColor = (type: string, chipBackground?: string, chipColor?: string) => {
+    const theme = useTheme();
+
     const getChipColor = () => {
         switch(type) {
+            case Tags.PUB:
+                return {
+                    background: 'background.default',
+                    border: `1px solid ${theme.palette.error.light}`,
+                    color: theme.palette.error.main
+                };
+            case Tags.VIDEO:
+                return {
+                    background: 'background.default',
+                    border: '1px solid #E0C2FF',
+                    color: '#9C87B2'
+                };
+            case Tags.INTERVIEW:
+                return {
+                    background: 'background.default',
+                    border: `1px solid ${theme.palette.warning.main}`,
+                    color: theme.palette.warning.main
+                };
             case Tags.LIVE:
             case Tags.REPLAY:
                 return {
