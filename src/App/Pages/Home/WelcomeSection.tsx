8d7e133c2848b12b97508b70f9c4b9ed02684d4f
HelviraG <pasiphae.code@gmail.com>
2023-12-14 07:41:28 +0100

    feat(WelcomeSection): complete section

--- src/App/Pages/Home/WelcomeSection.tsx
+++ src/App/Pages/Home/WelcomeSection.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import { WelcomeSectionBox, WelcomeText, WelcomeTextBox, WelcomeTextInnerBox } from '../../Styles/Pages/HomeStyle';
+import { Trans, useTranslation } from 'react-i18next';
 
 export const WelcomeSection = () => {
+    const { t } = useTranslation();
+
     return (
         <WelcomeSectionBox>
-            <WelcomeText>Welcome</WelcomeText>
+            <WelcomeText>{t('app.general.hello')}</WelcomeText>
             <WelcomeTextBox>
                 <WelcomeTextInnerBox>
                     <Typography sx={{ fontWeight: 500 }}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <Trans 
+                            i18nKey="app.general.welcome.p1"
+                            components={{ bold: <strong /> }}
+                        />
+                    </Typography>
+                    <Typography sx={{ fontWeight: 500, marginBottom: 1, marginTop: 1 }}>
+                        <Trans 
+                            i18nKey="app.general.welcome.p2"
+                            components={{ bold: <strong /> }}
+                        />
+                    </Typography>
+                    <Typography sx={{ fontWeight: 500 }}>
+                        <Trans 
+                            i18nKey="app.general.welcome.p3"
+                            components={{ bold: <strong /> }}
+                        />
                     </Typography>
                 </WelcomeTextInnerBox>
             </WelcomeTextBox>
