010d6a5585f116e3eb620635de0e860cf219b893
HelviraG <pasiphae.code@gmail.com>
2023-12-14 07:35:15 +0100

    feat(app): add cursor

--- src/App/Layout/Desktop/Desktop.tsx
+++ src/App/Layout/Desktop/Desktop.tsx
@@ -2,14 +2,17 @@ import React, { ReactNode } from 'react';
 import { Header } from '../../Components/Header/Header';
 import { Footer } from '../../Components/Footer/Footer';
 import { ScrollTopButton } from '../../Components/ScrollTopButton/ScrollTopButton';
+import MouseContextProvider from '../../../Context/MouseContextProvider';
+import { Cursor } from '../../../Components/Cursor/Cursor';
 
 export const Desktop = ({ children }: { children: ReactNode }) => {
     return (
-        <>
+        <MouseContextProvider>
+            <Cursor />
             <Header />
             {children}
             <ScrollTopButton />
             <Footer />
-        </>
+        </MouseContextProvider>
     )
 };
