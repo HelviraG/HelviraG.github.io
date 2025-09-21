2fd3c14071ca29db33229af41f39cbaf3ed648c2
HelviraG <helvira.dev@gmail.com>
2025-09-21 19:27:52 +0200

    chore(app): update routes

--- client/src/Routes/Routes.tsx
+++ client/src/Routes/Routes.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { useMediaQuery } from '@mui/material';
-import { Routes, Route } from "react-router-dom";
-import { Conferences } from '../App/Pages/Conferences';
-import { Contact } from '../App/Pages/Contact';
-import { Home } from '../App/Pages/Home';
-import { Videos } from '../App/Pages/Videos';
-import { Press } from '../App/Pages/Press';
-import { Routes as Links } from '../App/Resources/Enums/Routes';
-import { Career } from '../App/Pages/Career';
+import { useMediaQuery } from "@mui/material";
+import { Career } from "@pages/Career";
+import { Conferences } from "@pages/Conferences";
+import { Contact } from "@pages/Contact";
+import { Explorer } from "@pages/Explorer";
+import { Home } from "@pages/Home";
+import { Press } from "@pages/Press";
+import { Videos } from "@pages/Videos";
+import { Routes as Links } from "@resources/Enums/Routes";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
-    const isTablet = useMediaQuery('(max-width: 768px)');
+  const isTablet = useMediaQuery("(max-width: 768px)");
 
-    return (
-        <Routes>
-            <Route path={Links.CAREER} element={<Career isTablet={isTablet} />} />
-            <Route path={Links.CONFS} element={<Conferences isTablet={isTablet} />} />
-            <Route path={Links.CONTACT} element={<Contact isTablet={isTablet} />} />
-            <Route path={Links.HOME} element={<Home isTablet={isTablet} />} />
-            <Route path={Links.LIVE} element={<Videos isTablet={isTablet} />} />
-            <Route path={Links.PRESS} element={<Press isTablet={isTablet} />} />
-        </Routes>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path={Links.CAREER} element={<Career isTablet={isTablet} />} />
+        <Route
+          path={Links.CONFS}
+          element={<Conferences isTablet={isTablet} />}
+        />
+        <Route path={Links.CONTACT} element={<Contact isTablet={isTablet} />} />
+        <Route path={Links.HOME} element={<Home isTablet={isTablet} />} />
+        <Route path={Links.LIVE} element={<Videos isTablet={isTablet} />} />
+        <Route path={Links.PRESS} element={<Press isTablet={isTablet} />} />
+        <Route
+          path={Links.EXPLORE}
+          element={<Explorer isTablet={isTablet} />}
+        />
+      </Routes>
+    </>
+  );
+};
