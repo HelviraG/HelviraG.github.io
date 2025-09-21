791301b70440de58874d3ef2a1a70161f44833a2
HelviraG <helvira.dev@gmail.com>
2025-09-21 19:39:01 +0200

    chore(app): new home page

--- client/src/App/Pages/Home.tsx
+++ client/src/App/Pages/Home.tsx
@@ -1,46 +1,28 @@
-import React from 'react';
-import { Mobile } from '../Layout/Mobile/Mobile';
-import { MainSection } from './Home/MainSection';
-import { Desktop } from '../Layout/Desktop/Desktop';
-import { Masonry } from './Home/MasonryImageList';
-import { Box } from '@mui/material';
-import { WelcomeSection } from './Home/WelcomeSection';
-import { useTranslation } from 'react-i18next';
-import useDocumentTitle from '../../Hooks/useDocumentTitle';
-import { Types } from '../Resources/Enums/Types';
-import { HomeCTA } from './Home/HomeCTA';
+import useDocumentTitle from "@hooks/useDocumentTitle";
+import { useListConferencesQuery } from "@services/ConferencesApi";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Desktop } from "../Layout/Desktop/Desktop";
+import { Mobile } from "../Layout/Mobile/Mobile";
+import { MainSection } from "./Home/MainSection";
 
 export const Home = ({ isTablet }: { isTablet: boolean }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const listConfs = useListConferencesQuery();
 
-    useDocumentTitle(`Helvira Goma | ${t('app.menu.home')}`);
+  useDocumentTitle(`Helvira Goma | 🖲️ ${t("app.menu.home")}`);
 
-    return (
-        <>
-            {isTablet ? (
-                <Mobile>
-                    <MainSection />
-                    <Masonry anchor={Types.TOP} />
-                    <WelcomeSection />
-                    <HomeCTA />
-                    <Masonry anchor={Types.BOTTOM} />
-                </Mobile>
-            ) : (
-                <Desktop>
-                    <MainSection />
-                    <Box sx={(theme) => ({
-                        [theme.breakpoints.up('xl')]: {
-                            margin: '0 auto',
-                            maxWidth: '80%'
-                        },
-                    })}>
-                        <Masonry anchor={Types.TOP} />
-                        <WelcomeSection />
-                        <HomeCTA />
-                        <Masonry anchor={Types.BOTTOM} />
-                    </Box>
-                </Desktop>
-            )}
-        </>
-    )
-}
+  return (
+    <>
+      {isTablet ? (
+        <Mobile>
+          <MainSection />
+        </Mobile>
+      ) : (
+        <Desktop>
+          <MainSection />
+        </Desktop>
+      )}
+    </>
+  );
+};
