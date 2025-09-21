3b6fd1283c9dec11db6d83847f2df79626fb8160
HelviraG <helvira.dev@gmail.com>
2025-09-21 19:33:12 +0200

    chore(app): add imagesn, routes, sounds & tags enums

---
+++ client/src/App/Resources/Enums/Images.ts
@@ -0,0 +1,27 @@
+export enum Covers {
+    DRAWER = 'https://i.ibb.co/WpjYtsv/helvira-g-a-computer-related-background-usable-as-a-wallpaper-i-46c333e3-7496-4ffb-bf64-1969c5a32f68.png',
+    HOME = 'https://i.ibb.co/d7FJhKm/20230531-200845.jpg?w=248&fit=crop&auto=format',
+    CONFS = 'https://i.ibb.co/3yd6VFP/helvira-g-an-old-microphone-tech-related-background-usable-as-a-022af011-4b49-4315-b2b8-df5ac52cb51a.png',
+    CAREER = 'https://i.ibb.co/ZW0LtfZ/Copie-de-GIT-INIT-9.png',
+    CONTACT = 'https://i.ibb.co/kJjLNfG/helvira-g-a-computer-related-background-usable-as-a-wallpaper-i-6ac9d76c-8749-4466-9d8f-025cd89eddff.png',
+    LIVE = 'https://i.ibb.co/8jXWvQb/helvira-g-an-old-video-camera-tech-related-background-usable-as-e45ab7b1-1db6-427d-a2d6-6a34a5a8a3ae.png',
+    EXPLORER = 'https://i.ibb.co/MCKF5xt/Whats-App-Image-2024-05-31-20-09-03-3a2ea81c-1.jpg',
+    PRESS = 'https://i.ibb.co/dDTbqgb/helvira-g-an-old-newspaper-in-a-pop-art-aead2e9a-b643-4269-b50c-cdab84b32232-2.png',
+}
+
+export enum Avatars {
+    DRAWER = 'https://i.ibb.co/dgfbNSf/1715867189847-1.jpg',
+    MENU = 'https://i.ibb.co/j6mWCyJ/Design-sans-titre-2.png',
+}
+
+export enum SubTitle {
+    HOME = 'https://i.ibb.co/ncPZztp/Copie-de-GIT-INIT-7-removebg-preview.png',
+    CONFS = 'https://i.ibb.co/YykggK8/Copie-de-GIT-INIT-10-removebg-preview.png',
+    CAREER = 'https://i.ibb.co/DDG91d9/Copie-de-GIT-INIT-15-removebg-preview.png',
+    CONTACT = 'https://i.ibb.co/GnhtYYf/Copie-de-GIT-INIT-10-removebg-preview-2.png',
+    LIVE = 'https://i.ibb.co/ZXtX0Zb/Copie-de-GIT-INIT-14-removebg-preview-1.png',
+    PASSION = 'https://i.ibb.co/dc4dJ3s/Copie-de-GIT-INIT-17-removebg-preview.png',
+    EXPLORER = 'https://i.ibb.co/3NZVMWj/Copie-de-GIT-INIT-8-removebg-preview.png',
+    PRESS = 'https://i.ibb.co/H2ZDRmv/Copie-de-GIT-INIT-16-removebg-preview.png',
+    SPEAKER = 'https://i.ibb.co/DWQYkb2/Copie-de-GIT-INIT-12-removebg-preview.png',
+}
---
+++ client/src/App/Resources/Enums/Sounds.ts
@@ -0,0 +1,10 @@
+export enum Sounds {
+    TAB_SETTINGS = 'https://od.lk/s/MzRfMzc3NDc5NDVf/90s-game-ui-10-185103.mp3',
+    CLICK_SETTINGS = 'https://od.lk/s/MzRfMzc3NDc4MDNf/old-radio-button-click-97549.mp3',
+    SLIDE_QUIZ = 'https://od.lk/s/MzRfMzc3NDgwNjFf/resonant-reverb-pop-layers-98330.mp3',
+    START_BACKGROUND = 'https://od.lk/s/MzRfMzc2ODA3MTFf/WhatsApp%20Audio%202024-06-22%20%C3%A0%2001.45.29_781b8c84.mp3',
+    QUIZ_BACKGROUND = 'https://od.lk/s/MzRfMzc3NDc4NTlf/space-ambient-sci-fi-121842.mp3',
+    SELECT_FIELD = 'https://od.lk/s/MzRfMzQ1MTkxODVf/select.mp3',
+    NAVIGATE_QUIZ = 'https://od.lk/s/MzRfMzQ1MTkxODhf/confirm.mp3',
+    START_QUIZ = 'https://od.lk/s/MzRfMzc3NDgwNjVf/scale-e6-14577.mp3'
+}
