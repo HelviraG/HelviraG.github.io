61e75856d31c8e0e5f82b1914c13cc98b2a5b273
HelviraG <helvira.dev@gmail.com>
2025-09-22 07:48:13 +0200

    chore(app): update conferences page style

--- client/src/App/Styles/Pages/ConferencesStyle.tsx
+++ client/src/App/Styles/Pages/ConferencesStyle.tsx
@@ -1,4 +1,12 @@
-import { Box, styled, BoxProps } from '@mui/material';
+import {Box, styled, BoxProps, CardMedia, CardMediaProps, Accordion, AccordionSummary} from '@mui/material';
+
+interface CardContentMediaProps extends CardMediaProps {
+    imgUrl?: string;
+}
+
+interface EventBoxProps extends BoxProps {
+    isConference?: boolean;
+}
 
 interface DateWrapperProps extends BoxProps {
     darkColor: string;
@@ -15,17 +23,29 @@ export const ChipWrapper = styled(Box)(({ theme }) => ({
     }
 }));
 
-export const EventBox = styled(Box)(({ theme }) => ({
+export const EventBox = styled(Box, {
+    shouldForwardProp: (prop) => prop !== 'isConference',
+})<EventBoxProps>(({ isConference, theme }) => ({
     [theme.breakpoints.down('md')]: {
         display: 'flex',
         alignItems: 'center',
     },
+
+    ...(isConference && {
+        width: '100px'
+    })
 }));
 
-export const ItemEvent = styled(Box)(({ theme }) => ({
+export const ItemEvent = styled(Box, {
+    shouldForwardProp: (prop) => prop !== 'isConference',
+})<EventBoxProps>(({ isConference, theme }) => ({
     display: 'flex', 
     flexDirection: 'row-reverse', 
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
+
+    ...(isConference && {
+        marginTop: 0
+    })
 }));
 
 export const ItemEventWrapper = styled(Box)(({ theme }) => ({
@@ -70,3 +90,106 @@ export const EventLocationWrapper = styled(Box)(({ theme }) => ({
     display: 'flex', 
     gap: theme.spacing(.5)
 }));
+
+export const CardEventMedia = styled(CardMedia, {
+    shouldForwardProp: (prop) => prop !== 'imgUrl',
+})<CardContentMediaProps>(({ imgUrl, theme }) => ({
+    backgroundColor: 'background.default',
+    backgroundImage: `url(${imgUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'bottom',
+    borderRadius: theme.spacing(1),
+    height: 0,
+    marginLeft: '-24px',
+    marginRight: 'auto',
+    marginTop: '-24px',
+    paddingBottom: '48%',
+    position: 'relative',
+    transform: 'translateX(-50px)',
+    width: '100%',
+
+    [theme.breakpoints.down('md')]: {
+        backgroundPosition: 'center',
+        marginLeft: '26px',
+        marginRight: 'auto',
+        marginTop: '-74px',
+        width: '90%',
+    },
+
+    [theme.breakpoints.down('lg')]: {
+        transform: 'translateX(-24px)',
+    },
+
+    '&:after': {
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        content: '""',
+        opacity: '0.4',
+        position: 'absolute',
+        borderRadius: theme.spacing(1),
+        backgroundImage: 'linear-gradient(90deg, #f783ac 5%, #7950f2 95%)',
+    }
+}));
+
+export const ConferenceCardWrapper = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '100%',
+
+    [theme.breakpoints.down(500)]: {
+        maxWidth: '90%', margin: '0 auto'
+    }
+}));
+
+export const ConferenceCardHeader = styled(Box)(({ theme }) => ({
+    padding: '0 10px 0 0',
+
+    [theme.breakpoints.down(500)]: {
+        marginTop: theme.spacing(3)
+    }
+}));
+
+export const ConferenceCardBody = styled(Box)(({
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '20px',
+    gap: '20px'
+}));
+
+export const CardEvent = styled(Accordion)(({ theme }) => ({
+    maxWidth: '90%',
+
+    [theme.breakpoints.down(500)]: {
+        maxWidth: '100%'
+    }
+}));
+
+export const CardEventDetailHeader = styled(AccordionSummary)(({ theme }) => ({
+    width: '100%',
+
+    '.accordion-summary-box': {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        gap: '4px',
+
+        '.accordion-summary-box-item1': {
+            alignSelf: 'end'
+        },
+
+        '.accordion-summary-box-item2': {
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '-webkit-fill-available',
+
+            '.accordion-summary-box-item2-actions': {
+                display: 'flex',
+                alignItems: 'center',
+                gap: '2px'
+            }
+        }
+    }
+}))
