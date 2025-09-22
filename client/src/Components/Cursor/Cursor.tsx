12a180365699917d8e3ee4b12c5a8814e7c5e06c
HelviraG <helvira.dev@gmail.com>
2025-09-22 07:25:46 +0200

    chore(app): update cursor to mui

--- client/src/Components/Cursor/Cursor.tsx
+++ client/src/Components/Cursor/Cursor.tsx
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import "./DotRing.css";
 import useMousePosition from "../../Hooks/useMousePosition";
-import { MouseContext } from '../../Context/MouseContextProvider';
+import { MouseContext } from '@context/MouseContextProvider';
 import { useMediaQuery } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { Routes } from '@resources/Enums/Routes';
+import { alpha, Box } from '@mui/material';
 
 export const Cursor = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+
   const { cursorType } = useContext(MouseContext);
   const { x, y } = useMousePosition();
 
@@ -12,14 +18,35 @@ export const Cursor = () => {
 
   return (
     <>
-      <div
-        style={{ left: `${x}px`, top: `${y}px`, display: isTablet ? 'none' : 'block' }}
+      <Box
+        sx={{
+            ...(pathname.startsWith(Routes.EXPLORE) && {
+                background: 'linear-gradient(to right, #3AE374 2px, transparent 2px) 0 0, linear-gradient(to right, #3AE374 2px, transparent 2px) 0 100%, linear-gradient(to left, #3AE374 2px, transparent 2px) 100% 0, linear-gradient(to left, #3AE374 2px, transparent 2px) 100% 100%, linear-gradient(to bottom, #3AE374 2px, transparent 2px) 0 0, linear-gradient(to bottom, #3AE374 2px, transparent 2px) 100% 0, linear-gradient(to top, #3AE374 2px, transparent 2px) 0 100%, linear-gradient(to top, #3AE374 2px, transparent 2px) 100% 100%',
+            }),
+            ...(!pathname.startsWith(Routes.EXPLORE) && {
+                background: 'linear-gradient(to right, #FF4757 2px, transparent 2px) 0 0, linear-gradient(to right, #FF4757 2px, transparent 2px) 0 100%, linear-gradient(to left, #FF4757 2px, transparent 2px) 100% 0, linear-gradient(to left, #FF4757 2px, transparent 2px) 100% 100%, linear-gradient(to bottom, #FF4757 2px, transparent 2px) 0 0, linear-gradient(to bottom, #FF4757 2px, transparent 2px) 100% 0, linear-gradient(to top, #FF4757 2px, transparent 2px) 0 100%, linear-gradient(to top, #FF4757 2px, transparent 2px) 100% 100%',
+            }),
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: '6px 8px',
+            backgroundColor: pathname.startsWith(Routes.EXPLORE) ? alpha('#3AE374', .2) : alpha('#FF4757', .2) ,
+            borderRadius: '4px',
+            display: isTablet ? 'none' : 'block',
+            left: `${x}px`,
+            top: `${y}px`,
+
+            zIndex: 9999999999
+        }}
         className={"ring " + cursorType}
-      ></div>
-      <div
+      ></Box>
+      <Box
         className={"dot " + cursorType}
-        style={{ left: `${x}px`, top: `${y}px`, display: isTablet ? 'none' : 'block'  }}
-      ></div>
+        sx={{
+          left: `${x}px`, top: `${y}px`,
+          display: isTablet ? 'none' : 'block',
+          backgroundColor: pathname.startsWith(Routes.EXPLORE) ? '#32FF7E' : '#FF4757',
+          zIndex: 9999999999
+        }}
+      ></Box>
     </>
   );
 };
