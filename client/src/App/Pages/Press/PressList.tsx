a42f6ec11edd1aca67c33dd51f0a15d924d2bfb4
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:05:14 +0200

    chore(app): update press page components

--- client/src/App/Pages/Press/PressList.tsx
+++ client/src/App/Pages/Press/PressList.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import { ListItemsWrapper, ListTitleWrapper, ListWrapper } from '../../Styles/Components/List/List';
-import { SubTitleTypography, TitleTypography, TitleWrapper } from '../../Styles/Layout/TitleStyle';
-import { useTranslation } from 'react-i18next';
-import { IPress, ListPressArticles } from '../../Resources/PressResource';
-import { PressListItem } from './PressListItem';
+import { SubTitle } from "@resources/Enums/Images";
+import {
+  IPress,
+  ListPressArticles,
+} from "@resources/Pages/General/PressResource";
+import {
+  ListItemsWrapper,
+  ListTitleWrapper,
+  ListWrapper,
+} from "@styles/Components/List/List";
+import {
+  SubTitleTypography,
+  TitleTypography,
+  TitleWrapper,
+} from "@styles/Layout/TitleStyle";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { PressListItem } from "./PressListItem";
 
 export const PressList = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <ListWrapper>
-            <ListTitleWrapper>
-                <TitleWrapper>
-                    <TitleTypography variant='h3'>{t('app.press.subtitle')}</TitleTypography>
-                    <SubTitleTypography>{t('app.press.description')}</SubTitleTypography>
-                </TitleWrapper>
-            </ListTitleWrapper>
-            <ListItemsWrapper className="press-list">
-                {ListPressArticles().map((article: IPress) => (
-                    <PressListItem {...article} key={`${article.title}`} />
-                ))}
-            </ListItemsWrapper>
-        </ListWrapper>
-    )
-}
+  return (
+    <ListWrapper>
+      <ListTitleWrapper>
+        <TitleWrapper textAlign="right">
+          <img src={SubTitle.PRESS} alt="Press page subtitle icon" />
+          <TitleTypography variant="h3">
+            {t("app.press.subtitle")}
+          </TitleTypography>
+        </TitleWrapper>
+        <SubTitleTypography>{t("app.press.description")}</SubTitleTypography>
+      </ListTitleWrapper>
+      <ListItemsWrapper className="press-list">
+        {ListPressArticles().map((article: IPress) => (
+          <PressListItem {...article} key={`${article.title}`} />
+        ))}
+      </ListItemsWrapper>
+    </ListWrapper>
+  );
+};
--- client/src/App/Pages/Press/PressListItem.tsx
+++ client/src/App/Pages/Press/PressListItem.tsx
@@ -1,51 +1,61 @@
-import React from 'react';
-import { IPress } from '../../Resources/PressResource';
-import { ChipWrapper } from '../../Styles/Pages/ConferencesStyle';
-import { AppChip } from '../../Components/Chip/Chip';
-import { CardContentSubtitle, CardContentButton, CardContentAction, CardContentCaption, CardContentTitle, CardContentWrapper, CardWrapper, CardContentMedia } from '../../Styles/Components/List/ListItem';
-import { useTranslation } from 'react-i18next';
-import FeedRoundedIcon from '@mui/icons-material/FeedRounded';
+import { AppChip } from "@component/Chip/Chip";
+import FeedRoundedIcon from "@mui/icons-material/FeedRounded";
+import { IPress } from "@resources/Pages/General/PressResource";
+import {
+  CardContentAction,
+  CardContentButton,
+  CardContentCaption,
+  CardContentMedia,
+  CardContentSubtitle,
+  CardContentTitle,
+  CardContentWrapper,
+  CardWrapper,
+} from "@styles/Components/List/ListItem";
+import { ChipWrapper } from "@styles/Pages/ConferencesStyle";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-export const PressListItem = ({ 
-    media, 
-    mediaImg, 
-    title, 
-    tags, 
-    imgUrl, 
-    lightColor, 
-    darkColor,
-    link,
-    writtenBy,
-    caption
+export const PressListItem = ({
+  media,
+  title,
+  tags,
+  imgUrl,
+  lightColor,
+  darkColor,
+  link,
+  writtenBy,
+  caption,
 }: IPress) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <CardWrapper>
-            <CardContentMedia imgUrl={imgUrl} />
-            <CardContentWrapper>
-                <ChipWrapper>
-                    {tags.map((tag) => (
-                        <AppChip 
-                            chipBackground={lightColor}
-                            chipBorder={darkColor}
-                            chipColor={darkColor}
-                            size="small" 
-                            label={tag} 
-                            type={tag} 
-                            key={tag}
-                        />
-                    ))}
-                </ChipWrapper>
-                <CardContentSubtitle variant="caption">{writtenBy} | {media}</CardContentSubtitle>
-                <CardContentTitle variant="h4">{title}</CardContentTitle>
-                <CardContentCaption>{caption}</CardContentCaption>
-                <CardContentAction>
-                    <CardContentButton href={link} startIcon={<FeedRoundedIcon />}>
-                        {t('app.general.actions.read_more')}
-                    </CardContentButton>
-                </CardContentAction>
-            </CardContentWrapper>
-        </CardWrapper>
-    )
-}
+  return (
+    <CardWrapper>
+      <CardContentMedia component={"span"} imgUrl={imgUrl} />
+      <CardContentWrapper>
+        <ChipWrapper>
+          {tags.map((tag) => (
+            <AppChip
+              chipBackground={lightColor}
+              chipBorder={darkColor}
+              chipColor={darkColor}
+              size="small"
+              label={tag}
+              type={tag}
+              key={tag}
+            />
+          ))}
+        </ChipWrapper>
+        <CardContentSubtitle variant="caption">
+          {writtenBy} | {media}
+        </CardContentSubtitle>
+        <CardContentTitle variant="h4">{title}</CardContentTitle>
+        <CardContentCaption>{caption}</CardContentCaption>
+        <CardContentAction>
+          <CardContentButton href={link} startIcon={<FeedRoundedIcon />}>
+            {t("app.general.actions.read_more")}
+          </CardContentButton>
+        </CardContentAction>
+      </CardContentWrapper>
+    </CardWrapper>
+  );
+};
