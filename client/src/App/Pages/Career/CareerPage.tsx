dc90229434d0c78d4b3a958765549ad05caa6f32
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:11:33 +0200

    chore(app): update career page

--- client/src/App/Pages/Career/CareerPage.tsx
+++ client/src/App/Pages/Career/CareerPage.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import { ListTitleWrapper, ListWrapper } from '../../Styles/Components/List/List';
-import { SubTitleTypography, TitleTypography, TitleWrapper } from '../../Styles/Layout/TitleStyle';
-import { useTranslation } from 'react-i18next';
-import { CareerTimeline } from './CareerTimeline';
+import { SubTitle } from "@resources/Enums/Images";
+import { ListTitleWrapper, ListWrapper } from "@styles/Components/List/List";
+import {
+  SubTitleTypography,
+  TitleTypography,
+  TitleWrapper,
+} from "@styles/Layout/TitleStyle";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { CareerTimeline } from "./CareerTimeline";
 
 export const CareerPage = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <ListWrapper>
-            <ListTitleWrapper>
-                <TitleWrapper>
-                    <TitleTypography variant='h3'>{t('app.career.subtitle')}</TitleTypography>
-                    <SubTitleTypography>{t('app.career.description')}</SubTitleTypography>
-                </TitleWrapper>
-            </ListTitleWrapper>
-            <CareerTimeline />
-        </ListWrapper>
-    )
-}
+  return (
+    <ListWrapper>
+      <ListTitleWrapper>
+        <TitleWrapper textAlign="right">
+          <img src={SubTitle.CAREER} alt="Career page subtitle icon" />
+          <TitleTypography variant="h3">
+            {t("app.career.subtitle")}
+          </TitleTypography>
+        </TitleWrapper>
+        <SubTitleTypography>{t("app.career.description")}</SubTitleTypography>
+      </ListTitleWrapper>
+      <CareerTimeline />
+    </ListWrapper>
+  );
+};
