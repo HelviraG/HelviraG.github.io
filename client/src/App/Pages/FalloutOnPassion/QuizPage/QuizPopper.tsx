8fc253c06cb17f3df6afeb5dde3d8b0ec3fd6afe
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:25:32 +0200

    feat(fallout-passion): add quiz settings popper

---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizPopper.tsx
@@ -0,0 +1,38 @@
+import {QuizPopperContent, QuizPopperStyle, QuizPopperWrapper} from "@fallout/QuizPage/Styles/QuizPopper";
+import {Sounds} from "@resources/Enums/Sounds";
+import {MutableRefObject, SyntheticEvent, useState} from "react";
+import useSound from "use-sound";
+import {TabNavigation} from "./QuizSettingsPopper/TabNavigation";
+import {TabPanel} from "./QuizSettingsPopper/TabPanel";
+
+interface QuizPopperProps {
+    openSettings: boolean;
+    anchorEl: HTMLButtonElement | null;
+    popperRef: MutableRefObject<null>;
+}
+
+export const QuizPopper = ({openSettings, anchorEl, popperRef}: QuizPopperProps) => {
+    const [value, setValue] = useState(0);
+    const [play] = useSound(Sounds.CLICK_SETTINGS);
+
+    const handleTabNavigation = (event: SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+        play();
+    };
+
+    return (
+        <QuizPopperStyle
+            open={openSettings}
+            placement="bottom-end"
+            anchorEl={anchorEl}
+            ref={popperRef}
+        >
+            <QuizPopperWrapper>
+                <QuizPopperContent>
+                    <TabNavigation value={value} handleTabNavigation={handleTabNavigation}/>
+                    <TabPanel value={value}/>
+                </QuizPopperContent>
+            </QuizPopperWrapper>
+        </QuizPopperStyle>
+    )
+}
