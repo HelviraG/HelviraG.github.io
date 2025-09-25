9da8d47ed87024e49d4f7a77fe7ea8523867a77e
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:03:58 +0200

    chore(app): update videos page design

--- client/src/App/Pages/Videos.tsx
+++ client/src/App/Pages/Videos.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import { Mobile } from '../Layout/Mobile/Mobile';
-import { Desktop } from '../Layout/Desktop/Desktop';
-import { HeroTitle } from '../Components/Hero/HeroTitle';
-import { useTranslation } from 'react-i18next';
-import { VideosList } from './Videos/VideosList';
-import useDocumentTitle from '../../Hooks/useDocumentTitle';
+import { HeroTitle } from "@component/Hero/HeroTitle";
+import { Covers } from "@resources/Enums/Images";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import useDocumentTitle from "../../Hooks/useDocumentTitle";
+import { Desktop } from "../Layout/Desktop/Desktop";
+import { Mobile } from "../Layout/Mobile/Mobile";
+import { BuyMeACoffee } from "./Home/BuyMeACoffee";
+import { VideosList } from "./Videos/VideosList";
 
 export const Videos = ({ isTablet }: { isTablet: boolean }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    useDocumentTitle(`Helvira Goma | ${t('app.menu.live')}`);
-    
-    return (
-        <>
-            {isTablet ? (
-                <Mobile>
-                    <HeroTitle quote={t('app.quotes.lives')} title={t('app.lives.title')} titleColor="primary" />
-                    <VideosList />
-                </Mobile>
-            ) : (
-                <Desktop>
-                    <HeroTitle quote={t('app.quotes.lives')} title={t('app.lives.title')} titleColor="primary" />
-                    <VideosList />
-                </Desktop>
-            )}
-        </>
-    )
-}
+  useDocumentTitle(`Helvira Goma | 📼 ${t("app.menu.live")}`);
+
+  return (
+    <>
+      {isTablet ? (
+        <Mobile>
+          <HeroTitle
+            imgUrl={Covers.LIVE}
+            quote={t("app.quotes.lives")}
+            title={t("app.lives.title")}
+            titleColor="primary"
+          />
+          <VideosList />
+          <BuyMeACoffee />
+        </Mobile>
+      ) : (
+        <Desktop>
+          <HeroTitle
+            imgUrl={Covers.LIVE}
+            quote={t("app.quotes.lives")}
+            title={t("app.lives.title")}
+            titleColor="primary"
+          />
+          <VideosList />
+          <BuyMeACoffee />
+        </Desktop>
+      )}
+    </>
+  );
+};
