e8a468a258b07e56eaa5d1bb14eb42417daa1918
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:04:16 +0200

    chore(app): update conferences page design

--- client/src/App/Pages/Conferences.tsx
+++ client/src/App/Pages/Conferences.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import { Mobile } from '../Layout/Mobile/Mobile';
-import { Desktop } from '../Layout/Desktop/Desktop';
-import { ConferencesList } from './Conferences/ConferencesList';
-import { HeroTitle } from '../Components/Hero/HeroTitle';
-import { useTranslation } from 'react-i18next';
-import useDocumentTitle from '../../Hooks/useDocumentTitle';
+import { HeroTitle } from "@component/Hero/HeroTitle";
+import useDocumentTitle from "@hooks/useDocumentTitle";
+import { Covers } from "@resources/Enums/Images";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Desktop } from "../Layout/Desktop/Desktop";
+import { Mobile } from "../Layout/Mobile/Mobile";
+import { ConferencesList } from "./Conferences/ConferencesList";
+import { BuyMeACoffee } from "./Home/BuyMeACoffee";
 
 export const Conferences = ({ isTablet }: { isTablet: boolean }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    useDocumentTitle(`Helvira Goma | ${t('app.menu.conferences')}`);
-    
-    return (
-        <>
-            {isTablet ? (
-                <Mobile>
-                    <HeroTitle quote={t('app.quotes.confs')} title={t('app.confs.title')} titleColor="primary" />
-                    <ConferencesList />
-                </Mobile>
-            ) : (
-                <Desktop>
-                    <HeroTitle quote={t('app.quotes.confs')} title={t('app.confs.title')} titleColor="primary" />
-                    <ConferencesList />
-                </Desktop>
-            )}
-        </>
-    )
-}
+  useDocumentTitle(`Helvira Goma | 🎤 ${t("app.menu.conferences")}`);
+
+  return (
+    <>
+      {isTablet ? (
+        <Mobile>
+          <HeroTitle
+            imgUrl={Covers.CONFS}
+            quote={t("app.quotes.confs")}
+            title={t("app.confs.title")}
+            titleColor="warning"
+          />
+          <ConferencesList />
+          <BuyMeACoffee />
+        </Mobile>
+      ) : (
+        <Desktop>
+          <HeroTitle
+            imgUrl={Covers.CONFS}
+            quote={t("app.quotes.confs")}
+            title={t("app.confs.title")}
+            titleColor="warning"
+          />
+          <ConferencesList />
+          <BuyMeACoffee />
+        </Desktop>
+      )}
+    </>
+  );
+};
