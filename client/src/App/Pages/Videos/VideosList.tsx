380ee30da8f3850a65ae3f727fdeab140873e813
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:05:03 +0200

    chore(app): update videos page components

--- client/src/App/Pages/Videos/VideosList.tsx
+++ client/src/App/Pages/Videos/VideosList.tsx
@@ -1,47 +1,70 @@
-import React, { SetStateAction, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { ListItemsWrapper, ListTitleWrapper, ListWrapper } from '../../Styles/Components/List/List';
-import { SubTitleTypography, TitleTypography, TitleWrapper } from '../../Styles/Layout/TitleStyle';
-import { ILive, ListVideos } from '../../Resources/VideosResource';
-import { VideosListItem } from './VideosListItem';
-import { Tab } from '@mui/material';
-import { Tags } from '../../Resources/Enums/Tags';
-import { VideoTabs } from '../../Styles/Pages/VideosStyle';
+import { Tab } from "@mui/material";
+import { SubTitle } from "@resources/Enums/Images";
+import { Tags } from "@resources/Enums/Tags";
+import { ILive, ListVideos } from "@resources/Pages/General/VideosResource";
+import {
+  ListItemsWrapper,
+  ListTitleWrapper,
+  ListWrapper,
+} from "@styles/Components/List/List";
+import {
+  SubTitleTypography,
+  TitleTypography,
+  TitleWrapper,
+} from "@styles/Layout/TitleStyle";
+import { VideoTabs } from "@styles/Pages/VideosStyle";
+import React, { SetStateAction, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { VideosListItem } from "./VideosListItem";
 
 export const VideosList = () => {
-    const [currentTab, setCurrentTab] = useState(Tags.ALL);
-    const { t } = useTranslation();
+  const [currentTab, setCurrentTab] = useState(Tags.ALL);
+  const { t } = useTranslation();
 
-    const handleFilterTag = (event: any, newTab: SetStateAction<Tags>) => {
-        setCurrentTab(newTab);
-    };
+  const handleFilterTag = (event: any, newTab: SetStateAction<Tags>) => {
+    setCurrentTab(newTab);
+  };
 
-    const livesCategories = [
-        { label: t('app.lives.tags.all'), value: Tags.ALL },
-        { label: Tags.INTERVIEW, value: Tags.INTERVIEW },
-        { label: Tags.LIVE, value: Tags.LIVE },
-        { label: t('app.lives.tags.testimony'), value: t('app.lives.tags.testimony') },
-        { label: t('app.lives.tags.videos'), value: Tags.VIDEO },
-    ];
+  const livesCategories = [
+    { label: t("app.lives.tags.all"), value: Tags.ALL },
+    { label: Tags.INTERVIEW, value: Tags.INTERVIEW },
+    { label: Tags.LIVE, value: Tags.LIVE },
+    {
+      label: t("app.lives.tags.testimony"),
+      value: t("app.lives.tags.testimony"),
+    },
+    { label: t("app.lives.tags.videos"), value: Tags.VIDEO },
+  ];
 
-    return (
-        <ListWrapper>
-            <ListTitleWrapper>
-                <TitleWrapper>
-                    <TitleTypography variant="h3">{t('app.lives.subtitle')}</TitleTypography>
-                </TitleWrapper>
-                <SubTitleTypography>{t('app.lives.description')}</SubTitleTypography>
-            </ListTitleWrapper>
-            <VideoTabs variant="scrollable" value={currentTab} onChange={handleFilterTag}>
-                {livesCategories.map((category) => (
-                    <Tab value={category.value} key={category.label} label={category.label} />
-                ))}
-            </VideoTabs>
-            <ListItemsWrapper>
-                {ListVideos().map((live: ILive) => (
-                    <VideosListItem value={currentTab} {...live} key={`${live.title}`} />
-                ))}
-            </ListItemsWrapper>
-        </ListWrapper>
-    )
-}
+  return (
+    <ListWrapper>
+      <ListTitleWrapper>
+        <TitleWrapper textAlign="right">
+          <img src={SubTitle.LIVE} alt="Live page subtitle icon" />
+          <TitleTypography variant="h3">
+            {t("app.lives.subtitle")}
+          </TitleTypography>
+        </TitleWrapper>
+        <SubTitleTypography>{t("app.lives.description")}</SubTitleTypography>
+      </ListTitleWrapper>
+      <VideoTabs
+        variant="scrollable"
+        value={currentTab}
+        onChange={handleFilterTag}
+      >
+        {livesCategories.map((category) => (
+          <Tab
+            value={category.value}
+            key={category.label}
+            label={category.label}
+          />
+        ))}
+      </VideoTabs>
+      <ListItemsWrapper>
+        {ListVideos().map((live: ILive) => (
+          <VideosListItem value={currentTab} {...live} key={live.link} />
+        ))}
+      </ListItemsWrapper>
+    </ListWrapper>
+  );
+};
--- client/src/App/Pages/Videos/VideosListItem.tsx
+++ client/src/App/Pages/Videos/VideosListItem.tsx
@@ -1,74 +1,83 @@
-import React, { useState } from 'react';
-import { ChipWrapper } from '../../Styles/Pages/ConferencesStyle';
-import { ILive } from '../../Resources/VideosResource';
-import { AppChip } from '../../Components/Chip/Chip';
-import { VideoDialog } from '../../Components/VideoDialog/VideoDialog';
-import PlayCircleuOtlineRoundedIcon from '@mui/icons-material/PlayCircleOutlineRounded';
-import { useTranslation } from 'react-i18next';
-import { 
-    CardContentSubtitle, 
-    CardContentButton, 
-    CardContentAction, 
-    CardContentTitle, 
-    CardContentWrapper, 
-    CardWrapper, 
-    CardContentMedia 
-} from '../../Styles/Components/List/ListItem';
+import { AppChip } from "@component/Chip/Chip";
+import { VideoDialog } from "@component/VideoDialog/VideoDialog";
+import PlayCircleuOtlineRoundedIcon from "@mui/icons-material/PlayCircleOutlineRounded";
+import { ILive } from "@resources/Pages/General/VideosResource";
+import {
+  CardContentAction,
+  CardContentButton,
+  CardContentMedia,
+  CardContentSubtitle,
+  CardContentTitle,
+  CardContentWrapper,
+  CardWrapper,
+} from "@styles/Components/List/ListItem";
+import { ChipWrapper } from "@styles/Pages/ConferencesStyle";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 interface LiveItemProps {
-    index?: number;
-    value?: string;
+  index?: number;
+  value?: string;
 }
 
-export const VideosListItem = ({ 
-    channel,
-    channelImg, 
-    title, 
-    tags, 
-    imgUrl, 
-    lightColor, 
-    darkColor,
-    link,
-    value
+export const VideosListItem = ({
+  channel,
+  title,
+  tags,
+  imgUrl,
+  lightColor,
+  darkColor,
+  link,
+  value,
 }: ILive & LiveItemProps) => {
-    const { t } = useTranslation();
-    const [showVideo, setShowVideo] = useState(false);
-    const hasTag = tags.filter((tag) => tag === value).length > 0;
+  const { t } = useTranslation();
+  const [showVideo, setShowVideo] = useState(false);
+  const hasTag = tags.filter((tag) => tag === value).length > 0;
 
-    const handleShowVideo = () => {
-        setShowVideo(true);
-    }
+  const handleShowVideo = () => {
+    setShowVideo(true);
+  };
 
-    const handleHideVideo = () => {
-        setShowVideo(false);
-    }
+  const handleHideVideo = () => {
+    setShowVideo(false);
+  };
 
-    return (
-        <CardWrapper role="tabpanel" hidden={!hasTag}>
-            <CardContentMedia imgUrl={imgUrl} />
-            <CardContentWrapper>
-                <ChipWrapper>
-                    {tags.map((tag) => (
-                        <AppChip 
-                            chipBackground={lightColor}
-                            chipBorder={darkColor}
-                            chipColor={darkColor}
-                            size="small" 
-                            label={tag} 
-                            type={tag} 
-                            key={tag}
-                        />
-                    ))}
-                </ChipWrapper>
-                <CardContentSubtitle variant='caption'>{channel}</CardContentSubtitle>
-                <CardContentTitle variant="h4">{title}</CardContentTitle>
-                <CardContentAction>
-                    <CardContentButton onClick={handleShowVideo} startIcon={<PlayCircleuOtlineRoundedIcon />}>
-                        {t('app.general.actions.watch')}
-                    </CardContentButton>
-                </CardContentAction>
-            </CardContentWrapper>
-            {link && showVideo && <VideoDialog title={title} open={showVideo} close={handleHideVideo} src={link} />}
-        </CardWrapper>
-    )
-}
+  return (
+    <CardWrapper role="tabpanel" hidden={!hasTag}>
+      <CardContentMedia component={"span"} imgUrl={imgUrl} />
+      <CardContentWrapper>
+        <ChipWrapper>
+          {tags.map((tag) => (
+            <AppChip
+              chipBackground={lightColor}
+              chipBorder={darkColor}
+              chipColor={darkColor}
+              size="small"
+              label={tag}
+              type={tag}
+              key={`${tag}--${tag}`}
+            />
+          ))}
+        </ChipWrapper>
+        <CardContentSubtitle variant="caption">{channel}</CardContentSubtitle>
+        <CardContentTitle variant="h4">{title}</CardContentTitle>
+        <CardContentAction>
+          <CardContentButton
+            onClick={handleShowVideo}
+            startIcon={<PlayCircleuOtlineRoundedIcon />}
+          >
+            {t("app.general.actions.watch")}
+          </CardContentButton>
+        </CardContentAction>
+      </CardContentWrapper>
+      {link && showVideo && (
+        <VideoDialog
+          title={title}
+          open={showVideo}
+          close={handleHideVideo}
+          src={link}
+        />
+      )}
+    </CardWrapper>
+  );
+};
