9d2a6d6e7ee18f72e2e525298ea47f50ce8d964c
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:10:18 +0200

    chore(app): update Menu component

--- client/src/App/Components/Header/Menu/Menu.tsx
+++ client/src/App/Components/Header/Menu/Menu.tsx
@@ -1,19 +1,20 @@
-import React, { useRef } from 'react';
+import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
+import FiberNewOutlinedIcon from '@mui/icons-material/FiberNewOutlined';
+import FilterCenterFocusRoundedIcon from '@mui/icons-material/FilterCenterFocusRounded';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+import NewspaperRoundedIcon from '@mui/icons-material/NewspaperRounded';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
+import WavingHandRoundedIcon from '@mui/icons-material/WavingHandRounded';
 import { Box, DrawerProps } from '@mui/material';
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
-import { RouterLink } from '../../../Styles/Components/RouterLink';
-import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
-import { MenuIconBox, MenuIconButton, MenuItem, MenuItemText, MenuList, MenuListIcon, MenuListItemButton, MenuWrapper } from '../../../Styles/Layout/Menu';
-import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
-import ForumRoundedIcon from '@mui/icons-material/ForumRounded';
-import FilterCenterFocusRoundedIcon from '@mui/icons-material/FilterCenterFocusRounded';
-import NewspaperRoundedIcon from '@mui/icons-material/NewspaperRounded';
-import WavingHandRoundedIcon from '@mui/icons-material/WavingHandRounded';
-import { Routes } from '../../../Resources/Enums/Routes';
-import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
-import useClickOutside from '../../../../Hooks/useClickOutside';
-import { Types } from '../../../Resources/Enums/Types';
+import useClickOutside from '@hooks/useClickOutside';
+import { Routes } from '@resources/Enums/Routes';
+import { Types } from '@resources/Enums/Types';
+import { RouterLink } from '@styles/Components/RouterLink';
+import { MenuIconBox, MenuIconButton, MenuItem, MenuItemText, MenuList, MenuListIcon, MenuListItemButton, MenuWrapper } from '@styles/Layout/Menu';
 
 interface MenuProps extends DrawerProps
 {
@@ -31,7 +32,7 @@ export const Menu = ({ openMenu, menuWidth, closeDrawer }: MenuProps) => {
     useClickOutside(drawerRef, closeDrawer);
  
     return (
-        <MenuWrapper anchor={Types.RIGHT} open={openMenu} menuWidth={menuWidth} PaperProps={{ ref: drawerRef }}>
+        <MenuWrapper anchor={Types.RIGHT} open={openMenu} menuWidth={menuWidth} PaperProps={{ ref: drawerRef }} menuColor={pathname === Routes.EXPLORE ? '#AF0667' : '#1E1E40'} ModalProps={{ keepMounted: true, disableScrollLock: true }}>
             <MenuIconBox>
                 <MenuIconButton onClick={closeDrawer}>
                     <HighlightOffRoundedIcon sx={{ color: 'background.default' }} />
@@ -76,10 +77,11 @@ export const Menu = ({ openMenu, menuWidth, closeDrawer }: MenuProps) => {
                             <MenuItemText primary={t('app.menu.career')} />
                         </MenuListItemButton>
                     </MenuItem>
-                    <MenuItem key={t('app.menu.contact')}>
-                        <MenuListItemButton component={RouterLink} {...{ to: Routes.CONTACT, isActive: pathname === Routes.CONTACT }}>
-                            <MenuListIcon><ForumRoundedIcon /></MenuListIcon>
-                            <MenuItemText primary={t('app.menu.contact')} />
+                    <MenuItem key={t('app.menu.explorer')}>
+                        <MenuListItemButton component={RouterLink} {...{ to: Routes.EXPLORE, isActive: pathname === Routes.EXPLORE }}>
+                            <MenuListIcon><SportsEsportsIcon /></MenuListIcon>
+                            <MenuItemText primary={t('app.menu.explorer')} />
+                            <MenuListIcon iconColor='#FF6EC7'><FiberNewOutlinedIcon fontSize='large' /></MenuListIcon>
                         </MenuListItemButton>
                     </MenuItem>
                 </MenuList>
