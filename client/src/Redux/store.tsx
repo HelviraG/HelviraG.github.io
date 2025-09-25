544fcb6e2e12f559bb1a7116b2b6515ceec66fff
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:12:08 +0200

    chore(app): update store middlewares

--- client/src/Redux/store.tsx
+++ client/src/Redux/store.tsx
@@ -1,14 +1,24 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import StorageSlice from "./Slices/StorageSlice";
+import { conferencesApi } from "./Services/ConferencesApi";
+import { videosApi } from "./Services/VideosApi";
+import { pressApi } from "./Services/PressApi";
+import { careerApi } from "./Services/CareerApi";
+import { quizSlice } from "./Slices/QuizSlice";
 
 export const store = configureStore({
     reducer: {
-        storage: StorageSlice
+        storage: StorageSlice,
+        conferencesApi: conferencesApi.reducer,
+        videosApi: videosApi.reducer,
+        pressApi: pressApi.reducer,
+        careerApi: careerApi.reducer,
+        quiz: quizSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
-    })
+    }).concat(conferencesApi.middleware, videosApi.middleware, pressApi.middleware, careerApi.middleware),
 });
 
 setupListeners(store.dispatch);
