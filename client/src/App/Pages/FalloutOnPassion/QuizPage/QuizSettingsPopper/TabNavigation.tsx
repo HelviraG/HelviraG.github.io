7713933fd7892d9a4a670c8116670e266d021646
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:29:03 +0200

    feat(fallout-passion): add quiz settings tabs panel & navigation

---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizSettingsPopper/TabNavigation.tsx
@@ -0,0 +1,63 @@
+import {
+  TabNavigation as TabNavigationStyle,
+  TabsNavigation,
+  TabsNavigationWrapper,
+} from "@fallout/QuizPage/QuizSettingsPopper/TabPanel/TabsPanelStyle";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import DoorSlidingIcon from "@mui/icons-material/DoorSliding";
+import MarkUnreadChatAltIcon from "@mui/icons-material/MarkUnreadChatAlt";
+import ShareLocationIcon from "@mui/icons-material/ShareLocation";
+import { useMediaQuery } from "@mui/material";
+import React, { SyntheticEvent } from "react";
+import { useTranslation } from "react-i18next";
+
+export const TabNavigation = ({
+  value,
+  handleTabNavigation,
+}: {
+  value: number;
+  handleTabNavigation: (event: SyntheticEvent, value: number) => void;
+}) => {
+  const { t } = useTranslation();
+  const isMobile = useMediaQuery("(max-width: 500px)");
+
+  return (
+    <TabsNavigationWrapper>
+      <TabsNavigation
+        value={value}
+        onChange={handleTabNavigation}
+        aria-label="quiz settings popper tabs"
+        centered
+        variant={"fullWidth"}
+      >
+        <TabNavigationStyle
+          icon={<ShareLocationIcon fontSize={"small"} />}
+          value={0}
+          label={
+            isMobile ? "" : t("app.explore.fallout_on_passion.tabs.tab.title_1")
+          }
+          wrapped
+          iconPosition={"start"}
+        />
+        <TabNavigationStyle
+          icon={<CopyrightIcon fontSize={"small"} />}
+          value={2}
+          label={
+            isMobile ? "" : t("app.explore.fallout_on_passion.tabs.tab.title_3")
+          }
+          wrapped
+          iconPosition={"start"}
+        />
+        <TabNavigationStyle
+          icon={<DoorSlidingIcon fontSize={"small"} />}
+          value={3}
+          label={
+            isMobile ? "" : t("app.explore.fallout_on_passion.tabs.tab.title_4")
+          }
+          wrapped
+          iconPosition={"start"}
+        />
+      </TabsNavigation>
+    </TabsNavigationWrapper>
+  );
+};
---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizSettingsPopper/TabPanel.tsx
@@ -0,0 +1,60 @@
+import { CopyrightPanel } from "@fallout/QuizPage/QuizSettingsPopper/TabPanel/CopyrightPanel";
+import { InfoPanel } from "@fallout/QuizPage/QuizSettingsPopper/TabPanel/InfoPanel";
+import { LinksPanel } from "@fallout/QuizPage/QuizSettingsPopper/TabPanel/LinksPanel";
+import { TabPanelWrapper } from "@fallout/QuizPage/QuizSettingsPopper/TabPanel/TabsPanelStyle";
+import { Box } from "@mui/material";
+import React, { ReactNode } from "react";
+
+interface TabPanelProps {
+  children?: ReactNode;
+  index: number;
+  value: number;
+}
+
+export function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`quiz-tab-panel-${index}`}
+      aria-labelledby={`quiz-tab-${index}`}
+      style={{
+        color: '#FFFF',
+        ...(value === index && {
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          height: "-webkit-fill-available",
+        }),
+      }}
+      {...other}
+    >
+      {value === index && (
+        <Box
+          sx={{
+            color: '#FFFF',
+            ...(value === 1 && {
+              display: "flex",
+              flexDirection: "column",
+              height: "-webkit-fill-available",
+            }),
+          }}
+        >
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+export const TabPanel = ({ value }: { value: number }) => {
+  return (
+    <TabPanelWrapper value={value}>
+      <InfoPanel value={value} />
+      <CopyrightPanel value={value} />
+      <LinksPanel value={value} />
+    </TabPanelWrapper>
+  );
+};
