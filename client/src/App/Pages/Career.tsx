1a186318e3bd93c403b28b03c9ab97d23bcea9f6
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:04:35 +0200

    chore(app): update career page design

--- client/src/App/Pages/Career.tsx
+++ client/src/App/Pages/Career.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Mobile } from '../Layout/Mobile/Mobile';
-import { Desktop } from '../Layout/Desktop/Desktop';
-import { HeroTitle } from '../Components/Hero/HeroTitle';
-import { CareerPage } from './Career/CareerPage';
-import useDocumentTitle from '../../Hooks/useDocumentTitle';
+import { HeroTitle } from "@component/Hero/HeroTitle";
+import { Covers } from "@resources/Enums/Images";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import useDocumentTitle from "../../Hooks/useDocumentTitle";
+import { Desktop } from "../Layout/Desktop/Desktop";
+import { Mobile } from "../Layout/Mobile/Mobile";
+import { CareerPage } from "./Career/CareerPage";
+import { BuyMeACoffee } from "./Home/BuyMeACoffee";
 
 export const Career = ({ isTablet }: { isTablet: boolean }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    useDocumentTitle(`Helvira Goma | ${t('app.menu.career')}`);
-    
-    return (
-        <>
-            {isTablet ? (
-                <Mobile>
-                    <HeroTitle quote="" title={t('app.career.title')} titleColor="secondary" />
-                    <CareerPage />
-                </Mobile>
-            ) : (
-                <Desktop>
-                    <HeroTitle quote="" title={t('app.career.title')} titleColor="secondary" />
-                    <CareerPage />
-                </Desktop>
-            )}
-        </>
-    )
-}
+  useDocumentTitle(`Helvira Goma | ${t("app.menu.career")}`);
+
+  return (
+    <>
+      {isTablet ? (
+        <Mobile>
+          <HeroTitle
+            imgUrl={Covers.CAREER}
+            quote={t("app.quotes.career")}
+            title={t("app.career.title")}
+            titleColor="error"
+          />
+          <CareerPage />
+          <BuyMeACoffee />
+        </Mobile>
+      ) : (
+        <Desktop>
+          <HeroTitle
+            imgUrl={Covers.CAREER}
+            quote={t("app.quotes.career")}
+            title={t("app.career.title")}
+            titleColor="error"
+          />
+          <CareerPage />
+          <BuyMeACoffee />
+        </Desktop>
+      )}
+    </>
+  );
+};
