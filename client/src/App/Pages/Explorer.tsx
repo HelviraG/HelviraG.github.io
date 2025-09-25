5c17b5f14a371b4cfab0834ec4e55229e89e0b5d
HelviraG <helvira.dev@gmail.com>
2025-09-25 11:11:16 +0200

    chore(app): add Explorer page

---
+++ client/src/App/Pages/Explorer.tsx
@@ -0,0 +1,43 @@
+import { HeroTitle } from "@component/Hero/HeroTitle";
+import useDocumentTitle from "@hooks/useDocumentTitle";
+import { Covers } from "@resources/Enums/Images";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Desktop } from "../Layout/Desktop/Desktop";
+import { Mobile } from "../Layout/Mobile/Mobile";
+import { ExplorerList } from "./Explorer/ExplorerList";
+import { BuyMeACoffee } from "./Home/BuyMeACoffee";
+
+export const Explorer = ({ isTablet }: { isTablet: boolean }) => {
+  const { t } = useTranslation();
+
+  useDocumentTitle(`Helvira Goma | 🕹️ ${t("app.menu.explorer")}`);
+
+  return (
+    <>
+      {isTablet ? (
+        <Mobile>
+          <HeroTitle
+            imgUrl={Covers.EXPLORER}
+            quote={t("app.quotes.game_title")}
+            title={t("app.explore.subtitle")}
+            titleColor="error"
+          />
+          <ExplorerList />
+          <BuyMeACoffee />
+        </Mobile>
+      ) : (
+        <Desktop>
+          <HeroTitle
+            imgUrl={Covers.EXPLORER}
+            quote={t("app.quotes.game_title")}
+            title={t("app.explore.subtitle")}
+            titleColor="error"
+          />
+          <ExplorerList />
+          <BuyMeACoffee />
+        </Desktop>
+      )}
+    </>
+  );
+};
