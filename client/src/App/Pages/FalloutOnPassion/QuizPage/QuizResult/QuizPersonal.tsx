9a894c5af3b99112af0c595fed8dab904cbc8302
HelviraG <helvira.dev@gmail.com>
2025-10-01 05:10:02 +0200

    chore(app): update quiz personal result page

---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizResult/QuizPersonal.tsx
@@ -0,0 +1,303 @@
+import AppShortcutIcon from "@mui/icons-material/AppShortcut";
+import BadgeIcon from "@mui/icons-material/Badge";
+import BalanceIcon from "@mui/icons-material/Balance";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import { Box, Tab, Typography, useMediaQuery } from "@mui/material";
+import {
+  QuiPersonalContentWrapper,
+  QuizPersonalContentBody,
+  QuizPersonalContentFooter,
+  QuizPersonalContentHeader,
+  QuizPersonalContentTabs,
+  QuizPersonalHeader,
+  QuizPersonalHeaderWrapper,
+  QuizPersonalImgBox,
+  QuizPersonalWrapperBox,
+  TabPanelProps,
+  TabPanelWrapper,
+} from "@pages/FalloutOnPassion/QuizPage/QuizResult/QuizPersonalStyle";
+import { listAllAnswers, listAnswers, showField } from "@slices/QuizSlice";
+import React, { useEffect, useState } from "react";
+import { Trans, useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import useQuizResult from "../../Hooks/useQuizResult";
+import { useStoreResultMutation } from "@/Redux/Services/QuizApi";
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function stripQuotes(str: string) {
+  try {
+    return JSON.parse(str);
+  } catch {
+    return str;
+  }
+}
+
+export const QuizPersonal = ({ cat }: { cat: string }) => {
+  const { t } = useTranslation();
+  const isMobile = useMediaQuery("(max-width: 767px)");
+  const dispatch = useDispatch();
+  const answers = useSelector(listAllAnswers);
+  const { file, type } = useQuizResult(answers, cat);
+  const subTitle = file ? file.result.subTitle : "";
+
+  const category = useSelector(showField);
+
+  const [value, setValue] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const result = (type: string) => {
+    let resultContent;
+
+    if (file) {
+      switch (type) {
+        case "citizen":
+          resultContent = file.result.citizen;
+          break;
+        case "superMutant":
+          resultContent = file.result.superMutant;
+          break;
+        case "paladin":
+          resultContent = file.result.paladin;
+          break;
+        default:
+          break;
+      }
+    }
+
+    return resultContent;
+  };
+
+  const [storeResult, { isError, error, isLoading }] = useStoreResultMutation();
+    
+  const handleSubmit = async () => {
+    if (submitted || !category) return; // 👈 double guard
+    if (answers.length < 17) return;
+
+    try {
+      await storeResult({ category, type }).unwrap();
+      setSubmitted(true);
+    } catch (error) {
+      console.error("Error storing result:", error);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(listAnswers());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!submitted && category && type) {
+      handleSubmit();
+    }
+  }, [category, type, submitted]);
+
+  if (!file || !type || !result(type) || !result || !answers || !subTitle)
+    return null;
+
+  return (
+    <QuizPersonalWrapperBox>
+      <QuizPersonalHeaderWrapper>
+        <QuizPersonalImgBox
+          imgUrl={t(`app.explore.fallout_on_passion.quiz.result.img.${type}`)}
+        ></QuizPersonalImgBox>
+        <QuizPersonalHeader>
+          <Typography variant={"h5"} align={"right"}>
+            {typeof result(type) === "object" && result(type) !== null ? (result(type) as any).title : ""}
+          </Typography>
+        </QuizPersonalHeader>
+      </QuizPersonalHeaderWrapper>
+      
+      <QuiPersonalContentWrapper>
+        <QuizPersonalContentBody>
+          <QuizPersonalContentHeader>
+            <Typography variant={"subtitle1"}>
+              <Trans
+                i18nKey={
+                  typeof result(type) === "object" && result(type) !== null
+                    ? (result(type) as { text: string }).text
+                    : ""
+                }
+                components={{
+                  bold: (
+                    <Typography
+                      component={"span"}
+                      sx={{ fontWeight: 900, color: "#341F97" }}
+                    />
+                  ),
+                }}
+              />
+            </Typography>
+          </QuizPersonalContentHeader>
+          <Box>
+            <Box>
+              <QuizPersonalContentTabs
+                orientation={isMobile ? "horizontal" : "horizontal"}
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                centered={isMobile}
+                variant="scrollable"
+                isMobile={isMobile}
+                tabValue={value}
+              >
+                <Tab icon={<BadgeIcon />} {...a11yProps(0)} />
+                <Tab label={<BalanceIcon />} {...a11yProps(1)} />
+                <Tab label={<AppShortcutIcon />} {...a11yProps(2)} />
+                <Tab label={<InsertEmoticonIcon />} {...a11yProps(3)} />
+                <Tab label={<TravelExploreIcon />} {...a11yProps(4)} />
+              </QuizPersonalContentTabs>
+            </Box>
+            <TabPanelWrapper>
+              <TabPanel value={value} index={0}>
+                <Typography variant="h6" align={"right"} sx={{ marginBottom: 2 }}>
+                  {t(
+                    `app.explore.fallout_on_passion.quiz.result.label.${stripQuotes(cat)}.approach`,
+                  )}
+                </Typography>
+                <Trans
+                  i18nKey={
+                    typeof result(type) === "object" && result(type) !== null
+                      ? (result(type) as { approach: string }).approach
+                      : ""
+                  }
+                  components={{
+                    bold: (
+                      <Typography
+                        component={"span"}
+                        sx={{ fontWeight: 900, color: "#4e4187" }}
+                      />
+                    ),
+                  }}
+                />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                <Typography variant="h6" align={"right"} sx={{ marginBottom: 2 }}>
+                  {t(
+                    `app.explore.fallout_on_passion.quiz.result.label.${stripQuotes(cat)}.work_life`,
+                  )}
+                </Typography>
+                <Trans
+                  i18nKey={
+                    typeof result(type) === "object" && result(type) !== null
+                      ? (result(type) as { work_life: string }).work_life
+                      : ""
+                  }
+                  components={{
+                    bold: (
+                      <Typography
+                        component={"span"}
+                        sx={{ fontWeight: 900, color: "#FC7339" }}
+                      />
+                    ),
+                  }}
+                />
+              </TabPanel>
+              <TabPanel value={value} index={2}>
+                <Typography variant="h6" align={"right"} sx={{ marginBottom: 2 }}>
+                  {t(
+                    `app.explore.fallout_on_passion.quiz.result.label.${stripQuotes(cat)}.favorite_tools`,
+                  )}
+                </Typography>
+                <Trans
+                  i18nKey={
+                    typeof result(type) === "object" && result(type) !== null
+                      ? (result(type) as { favorite_tools: string }).favorite_tools
+                      : ""
+                  }
+                  components={{
+                    bold: (
+                      <Typography
+                        component={"span"}
+                        sx={{ fontWeight: 900, color: "#5f27CD" }}
+                      />
+                    ),
+                  }}
+                />
+              </TabPanel>
+              <TabPanel value={value} index={3}>
+                <Typography variant="h6" align={"right"} sx={{ marginBottom: 2 }}>
+                  {t(
+                    `app.explore.fallout_on_passion.quiz.result.label.${stripQuotes(cat)}.key_traits`,
+                  )}
+                </Typography>
+                <Trans
+                  i18nKey={
+                    typeof result(type) === "object" && result(type) !== null
+                      ? (result(type) as { key_traits: string }).key_traits
+                      : ""
+                  }
+                  components={{
+                    bold: (
+                      <Typography
+                        component={"span"}
+                        sx={{ fontWeight: 900, color: "#be2edd" }}
+                      />
+                    ),
+                  }}
+                />
+              </TabPanel>
+              <TabPanel value={value} index={4}>
+                <Typography variant="h6" align={"right"} sx={{ marginBottom: 2 }}>
+                  {t(
+                    `app.explore.fallout_on_passion.quiz.result.label.${stripQuotes(cat)}.social_style`,
+                  )}
+                </Typography>
+                <Trans
+                  i18nKey={
+                    typeof result(type) === "object" && result(type) !== null
+                      ? (result(type) as { social_style: string }).social_style
+                      : ""
+                  }
+                  components={{
+                    bold: (
+                      <Typography
+                        component={"span"}
+                        sx={{ fontWeight: 900, color: "#1dd1a1" }}
+                      />
+                    ),
+                  }}
+                />
+              </TabPanel>
+            </TabPanelWrapper>
+          </Box>
+        </QuizPersonalContentBody>
+        <QuizPersonalContentFooter>
+          <Typography variant="caption" textAlign="center" fontStyle="italic">
+            {t(subTitle)}
+          </Typography>
+        </QuizPersonalContentFooter>
+      </QuiPersonalContentWrapper>
+    </QuizPersonalWrapperBox>
+  );
+};
---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizResult/QuizPersonalStyle.tsx
@@ -0,0 +1,249 @@
+import { Box, BoxProps, styled, Tabs, TabsProps } from "@mui/material";
+import React from "react";
+
+export interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+interface QuizPersonalImgBoxProps extends BoxProps {
+  imgUrl: string;
+}
+
+interface QuizPersonalTabsProps extends TabsProps {
+  isMobile: boolean;
+  tabValue: number;
+}
+
+export const QuizPersonalWrapperBox = styled(Box)(({ theme }) => ({
+  position: 'relative', 
+  display: 'flex', 
+  alignItems: 'center', 
+  justifyContent: 'center', 
+  flex: 1,
+
+  [theme.breakpoints.down(800)]: {
+    flexDirection: 'column',
+    flex: 0,
+    maxHeight: '718px !important',
+    overflowY: 'scroll',
+    justifyContent: 'flex-start',
+  },
+
+  [theme.breakpoints.down(500)]: {
+    maxHeight: '620px !important',
+  },
+
+  [theme.breakpoints.down(400)]: {
+    maxHeight: '600px !important',
+  },
+}));
+
+export const QuizPersonalHeaderWrapper = styled(Box)(({ theme }) => ({
+  padding: '2em', 
+  width: '60%',  
+
+  [theme.breakpoints.down(800)]: { 
+    width: '100%', 
+  },
+
+  [theme.breakpoints.down(500)]: {
+    padding: '0em 10px',
+  },
+}));
+
+export const QuizPersonalImgBox = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "imgUrl",
+})<QuizPersonalImgBoxProps>(({ theme, imgUrl }) => ({
+  background: `url(${imgUrl}) center no-repeat`,
+  backgroundSize: "cover",
+  padding: "196px",
+  marginTop: "-33px",
+  borderRadius: "24px 24px 0px 0px",
+  width: "100%",
+
+  [theme.breakpoints.down(800)]: {
+    backgroundSize: "contain",
+  },
+
+  [theme.breakpoints.down(500)]: {
+    backgroundSize: "cover",
+    padding: "170px",
+  },
+
+  [theme.breakpoints.down(350)]: {
+    padding: "150px",
+  },
+}));
+
+export const QuizPersonalHeader = styled(Box)(({ theme }) => ({
+  padding: `${theme.spacing(2)} ${theme.spacing(4)}`,
+
+  "& .MuiTypography-root": {
+    color: "#341F97",
+    fontStyle: "italic",
+    fontWeight: 900,
+  },
+}));
+
+export const QuiPersonalContentWrapper = styled(Box)(({ theme }) => ({
+  padding: "24px",
+  overflowY: "auto",
+  backgroundColor: '#F2E7FF', 
+  display: 'flex', 
+  height: '-webkit-fill-available', 
+  alignItems: 'center', 
+  flexDirection: 'column', 
+  justifyContent: 'center', 
+  width: '50%',
+
+  [theme.breakpoints.down(800)]: {
+    width: '100%',
+    padding: theme.spacing(2),
+    height: 'auto',
+    overflowY: 'visible',
+  },
+}));
+
+export const QuizPersonalContentHeader = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: theme.spacing(2),
+  padding: `${theme.spacing(0)} ${theme.spacing(4)}`,
+
+  "& .MuiTypography-root": {
+    textAlign: "justify",
+  },
+}));
+
+export const QuizPersonalContentBody = styled(Box)(({ theme }) => ({
+  flexGrow: 1,
+  display: "flex",
+  marginBottom: theme.spacing(2),
+  marginTop: theme.spacing(0.5),
+  width: "100%",
+  flexDirection: "column",
+  justifyContent: "center",
+  gap: theme.spacing(6),
+
+  [theme.breakpoints.down(767)]: {
+    flexDirection: "column",
+  },
+
+  "& #full-width-tabpanel-0": {
+    "& .MuiTypography-root:first-of-type": {
+      color: '#4e4187',
+    },
+  },
+
+  "& #full-width-tabpanel-1": {
+    "& .MuiTypography-root:first-of-type": {
+      color: theme.game.special.orange,
+    },
+  },
+
+  "& #full-width-tabpanel-2": {
+    "& .MuiTypography-root:first-of-type": {
+      color: "#5f27CD",
+    },
+  },
+
+  "& #full-width-tabpanel-3": {
+    "& .MuiTypography-root:first-of-type": {
+      color: "#be2edd",
+    },
+  },
+
+  "& #full-width-tabpanel-4": {
+    "& .MuiTypography-root:first-of-type": {
+      color: "#1dd1a1",
+    },
+  },
+}));
+
+export const QuizPersonalContentTabs = styled(Tabs, {
+  shouldForwardProp: (prop) => prop !== "isMobile" && prop !== "tabValue",
+})<QuizPersonalTabsProps>(({ tabValue, isMobile, theme }) => ({
+  borderRight: "none",
+  borderColor: "divider",
+  borderBottom: "none",
+  width: "100%",
+
+  ...(isMobile && {
+    borderRight: "none",
+  }),
+
+  ...(tabValue === 0 && {
+    "& .MuiTabs-indicator": {
+      backgroundColor: '#4e4187',
+    },
+
+    "& .MuiButtonBase-root.MuiTab-root.Mui-selected": {
+      color: '#4e4187',
+    },
+  }),
+
+  ...(tabValue === 1 && {
+    "& .MuiTabs-indicator": {
+      backgroundColor: theme.game.special.orange,
+    },
+
+    "& .MuiButtonBase-root.MuiTab-root.Mui-selected": {
+      color: theme.game.special.orange,
+    },
+  }),
+
+  ...(tabValue === 2 && {
+    "& .MuiTabs-indicator": {
+      backgroundColor: "#5f27CD",
+    },
+
+    "& .MuiButtonBase-root.MuiTab-root.Mui-selected": {
+      color: "#5f27CD",
+    },
+  }),
+
+  ...(tabValue === 3 && {
+    "& .MuiTabs-indicator": {
+      backgroundColor: "#be2edd",
+    },
+
+    "& .MuiButtonBase-root.MuiTab-root.Mui-selected": {
+      color: "#be2edd",
+    },
+  }),
+
+  ...(tabValue === 4 && {
+    "& .MuiTabs-indicator": {
+      backgroundColor: "#1dd1a1",
+    },
+
+    "& .MuiButtonBase-root.MuiTab-root.Mui-selected": {
+      color: "#1dd1a1",
+    },
+  }),
+
+  "& .MuiTabs-flexContainer": {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
+
+export const QuizPersonalContentFooter = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(2),
+}));
+
+export const TabPanelWrapper = styled(Box)(({ theme }) => ({
+  marginTop: '2em', 
+  borderRadius: '32px', 
+  padding: '2em', 
+  backgroundColor: '#F2E7FF', 
+  height: '280px',
+
+  [theme.breakpoints.down(500)]: { 
+    height: 'auto', 
+    padding: 0,
+  },
+}));
