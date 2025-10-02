2cef0dde6dc43759231e60a244fba76d9e98cabd
HelviraG <helvira.dev@gmail.com>
2025-10-02 02:17:02 +0200

    chore(app): update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,31 @@
 import react from "@vitejs/plugin-react-refresh";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3000,
-    fs: {
-      allow: ["..", "src", ".", "../server"],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: 3000,
+      fs: {
+        allow: ["..", "src", ".", "../server"],
+      },
+      proxy: {
+        "/api": {  
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
+      }
     },
-  },
-  build: {
-    outDir: "./build",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
+    build: {
+      outDir: "./build",
     },
-  },
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  }
 });
