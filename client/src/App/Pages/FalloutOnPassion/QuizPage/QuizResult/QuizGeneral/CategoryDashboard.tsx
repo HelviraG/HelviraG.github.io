38946b5b4a18a973953fc2b1bf82e8feb180e402
HelviraG <helvira.dev@gmail.com>
2025-10-02 02:16:49 +0200

    chore(app): add category & general quiz results sections

---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizResult/QuizGeneral/CategoryDashboard.tsx
@@ -0,0 +1,108 @@
+import { useListResultQuery } from "@/Redux/Services/QuizApi";
+import { pieArcLabelClasses, PieChart } from '@mui/x-charts/PieChart';
+import { Box, useMediaQuery } from "@mui/material"
+import { DefaultizedPieValueType } from "@mui/x-charts";
+import { 
+    ChartBox, 
+    DashboardContentChartWrapper, 
+    DashboardContentDivider, 
+    DashboardContentInner, 
+    DashboardContentInnerPill, 
+    DashboardContentInnerPillTypo, 
+    DashboardContentTypo, 
+    DashboardContentWrapper 
+} from "./QuizGeneralStyle";
+import { useTranslation } from "react-i18next";
+
+export const CategoryDashboard = ({ cat }: { cat: string }) => {
+    const { t } = useTranslation();
+    const { data } = useListResultQuery({ category: cat });
+    const isMobile = useMediaQuery("(max-width: 500px)")
+    
+    if (!data?.percentages) return null;
+
+    const categories: { key: 'citizen' | 'paladin' | 'superMutant', label: string, labelMarkType: 'square' }[] = [
+        { key: 'citizen', label: t('app.explore.fallout_on_passion.quiz.result.types.citizen'), labelMarkType: 'square' },
+        { key: 'paladin', label: t('app.explore.fallout_on_passion.quiz.result.types.paladin'), labelMarkType: 'square' },
+        { key: 'superMutant', label: t('app.explore.fallout_on_passion.quiz.result.types.superMutant'), labelMarkType: 'square' },
+    ];
+
+    const dataToChart = categories
+        .map(({ key, label, labelMarkType }) => ({
+            value: parseInt(data.percentages[key], 10),
+            label,
+            labelMarkType,
+        }))
+        .filter(item => item.value !== 0);
+
+    const getArcLabel = (params: DefaultizedPieValueType) => {
+        const percent = params.value;
+
+        return `${(percent).toFixed(0)}%`;
+    };
+
+    return (
+        <DashboardContentWrapper>
+            <DashboardContentInner>
+                <Box>
+                    <DashboardContentInnerPill>
+                        <DashboardContentInnerPillTypo>team {cat}</DashboardContentInnerPillTypo>
+                    </DashboardContentInnerPill>
+                </Box>
+                <Box>
+                    <DashboardContentTypo>{t('app.explore.fallout_on_passion.quiz.result.chart.subText')}</DashboardContentTypo>
+                    <DashboardContentDivider />
+                </Box>
+                <DashboardContentChartWrapper>
+                    <ChartBox>
+                        <PieChart
+                            series={[
+                                {
+                                    innerRadius: 30,
+                                    outerRadius: 100,
+                                    paddingAngle: 5,
+                                    cornerRadius: 10,
+                                    startAngle: -45,
+                                    endAngle: 280,
+                                    data: dataToChart,
+                                    arcLabel: getArcLabel
+                                },
+                            ]}
+                            sx={(theme) => ({
+                                '& .MuiChartsLegend-root': {
+                                    backgroundColor: theme.game.special.dark,
+                                    borderRadius: theme.spacing(3),
+                                    color: theme.palette.background.paper,
+                                    padding: '10px 12px'
+                                },
+
+                                [`& .${pieArcLabelClasses.root}`]: {
+                                    fill: 'white',
+                                    fontSize: 14,
+                                },
+                            })}
+                            slotProps={{
+                                noDataOverlay: { 
+                                    message: t('app.explore.fallout_on_passion.quiz.result.chart.noData'),
+                                    sx: {
+                                        fontSize: 18,
+                                        fontWeight: 'bold',
+                                        fill: 'white'
+                                    }
+                                },
+                                legend: {
+                                    direction: isMobile ? 'horizontal' : 'vertical',
+                                    position: { 
+                                        vertical: isMobile ? 'bottom' : 'middle',
+                                        horizontal: 'center'
+                                    }
+                                }
+                            }}
+                            height={250}
+                        />
+                    </ChartBox>
+                </DashboardContentChartWrapper>
+            </DashboardContentInner>
+        </DashboardContentWrapper>
+    )
+}
---
+++ client/src/App/Pages/FalloutOnPassion/QuizPage/QuizResult/QuizGeneral/GeneralDashboard.tsx
@@ -0,0 +1,99 @@
+import { useListResultQuery } from "@/Redux/Services/QuizApi";
+import { Box, useMediaQuery } from "@mui/material";
+import { DefaultizedPieValueType, pieArcLabelClasses, PieChart } from "@mui/x-charts";
+import { 
+    ChartBox,
+    DashboardContentChartWrapper, 
+    DashboardContentDivider, 
+    DashboardContentInner, 
+    DashboardContentInnerPill, 
+    DashboardContentInnerPillTypo, 
+    DashboardContentTypo, 
+    DashboardContentWrapper 
+} from "./QuizGeneralStyle";
+import { useTranslation } from "react-i18next";
+
+export const GeneralDashboard = () => {
+    const { t } = useTranslation();
+    const { data } = useListResultQuery({});
+    const isMobile = useMediaQuery("(max-width: 500px)")
+
+    if (!data?.percentages) return null;
+
+    const dataToChart = [
+        { value: parseInt(data.percentages.citizen, 10), label: t('app.explore.fallout_on_passion.quiz.result.types.citizen'), labelMarkType: 'square' as const },
+        { value: parseInt(data.percentages.paladin, 10), label: t('app.explore.fallout_on_passion.quiz.result.types.paladin'), labelMarkType: 'square' as const },
+        { value: parseInt(data.percentages.superMutant, 10), label: t('app.explore.fallout_on_passion.quiz.result.types.superMutant'), labelMarkType: 'square' as const },
+    ];
+
+    const getArcLabel = (params: DefaultizedPieValueType) => {
+        const percent = params.value;
+
+        return `${(percent).toFixed(0)}%`;
+    };
+
+    return (
+        <DashboardContentWrapper>
+            <DashboardContentInner>
+                <Box>
+                    <DashboardContentInnerPill>
+                        <DashboardContentInnerPillTypo>{t('app.explore.fallout_on_passion.quiz.result.chart.allCategory')}</DashboardContentInnerPillTypo>
+                    </DashboardContentInnerPill>
+                </Box>
+                <Box>
+                    <DashboardContentTypo>{t('app.explore.fallout_on_passion.quiz.result.chart.subText')}</DashboardContentTypo>
+                    <DashboardContentDivider />
+                </Box>
+                <DashboardContentChartWrapper>
+                    <ChartBox>
+                        <PieChart
+                            series={[
+                                {
+                                    innerRadius: 30,
+                                    outerRadius: 100,
+                                    paddingAngle: 5,
+                                    cornerRadius: 10,
+                                    startAngle: -45,
+                                    endAngle: 280,
+                                    data: dataToChart,
+                                    arcLabel: getArcLabel
+                                },
+                            ]}
+                            sx={(theme) => ({
+                                '& .MuiChartsLegend-root': {
+                                    backgroundColor: theme.game.special.dark,
+                                    borderRadius: theme.spacing(3),
+                                    color: theme.palette.background.paper,
+                                    padding: '10px 12px'
+                                },
+
+                                [`& .${pieArcLabelClasses.root}`]: {
+                                    fill: 'white',
+                                    fontSize: 14
+                                }
+                            })}
+                            slotProps={{
+                                noDataOverlay: { 
+                                    message: t('app.explore.fallout_on_passion.quiz.result.chart.noData'),
+                                    sx: {
+                                        fontSize: 18,
+                                        fontWeight: 'bold',
+                                        fill: 'white'
+                                    }
+                                },
+                                legend: {
+                                    direction: isMobile ? 'horizontal' : 'vertical',
+                                    position: { 
+                                        vertical: isMobile ? 'bottom' : 'middle',
+                                        horizontal: 'center'
+                                    }
+                                }
+                            }}
+                            height={250}
+                        />
+                    </ChartBox>
+                </DashboardContentChartWrapper>
+            </DashboardContentInner>
+        </DashboardContentWrapper>
+    );
+}
