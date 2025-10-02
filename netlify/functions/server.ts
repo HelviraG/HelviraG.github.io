cb9839781fc82ddf03ed87945394fc3c463e2ad5
HelviraG <helvira.dev@gmail.com>
2025-10-02 05:32:35 +0200

    chore(app): config update

--- netlify/functions/server.ts
+++ netlify/functions/server.ts
@@ -1,4 +1,4 @@
-import ServerlessHttp from "serverless-http";
-import app from "../../server/index";
+import serverless from "serverless-http";
+import app from "../../server/index"; // path to your Express app
 
-export const handler = ServerlessHttp(app);
+export const handler = serverless(app);
