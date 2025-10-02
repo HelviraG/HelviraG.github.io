02ff1ee6926d2d11da9710316362ae5b5dc8bfc7
HelviraG <helvira.dev@gmail.com>
2025-10-02 04:51:54 +0200

    chore(app): config update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react-refresh";
+import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -19,8 +20,9 @@ export default defineConfig(({ mode }) => {
         }
       }
     },
+    root:  path.resolve(__dirname),
     build: {
-      outDir: "build",
+      outDir: "dist",
     },
     resolve: {
       alias: {
