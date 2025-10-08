8bbade0c2868c96d7090f042232df075ad691220
HelviraG <helvira.dev@gmail.com>
2025-10-08 06:06:53 +0200

    chore(app): update app footer styling

--- client/src/App/Components/Footer/Footer.tsx
+++ client/src/App/Components/Footer/Footer.tsx
@@ -1,19 +1,20 @@
-import { Typography } from '@mui/material';
+import { Typography, useMediaQuery } from '@mui/material';
 import { FooterLang, FooterTextTypography, FooterTextWrapper, FooterToolbar, FooterWrapper, LangSwitch } from '@styles/Layout/Footer';
 import { DigitalClock } from '../DigitalClock/DigitalClock';
 import useChangeLangage from "@hooks/useChangeLangage";
 
 export const Footer = () => {
   const { appLang: lang, changeLang: changeLanguage } = useChangeLangage();
+  const isTablet = useMediaQuery("(max-width: 800px");
 
   return (
     <FooterWrapper component="footer" position="fixed" color="primary">
       <FooterToolbar>
         <DigitalClock />
         <FooterLang>
-          <Typography variant="caption">🇫🇷 FR</Typography>
+          <Typography variant="caption">🇫🇷{isTablet ? '' : ' FR'}</Typography>
           <LangSwitch checked={lang === 'en'} onChange={changeLanguage} />
-          <Typography variant="caption">🇺🇸 EN</Typography>
+          <Typography variant="caption">🇺🇸{isTablet ? '' : ' EN'}</Typography>
         </FooterLang>
         <FooterTextWrapper>
           <FooterTextTypography variant="caption">Made with ❤️ & code lines 👩‍💻 by helvira_g</FooterTextTypography>
