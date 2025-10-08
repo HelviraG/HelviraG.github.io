d4b65b6b50d8a20fe8cf15c82edeadcef0423a1b
HelviraG <helvira.dev@gmail.com>
2025-10-08 03:05:18 +0200

    chore(app): add missing Tags cases for ChipColor

--- client/src/App/Styles/Components/ChipColor.tsx
+++ client/src/App/Styles/Components/ChipColor.tsx
@@ -12,17 +12,27 @@ export const ChipColor = (type: string, chipBackground?: string, chipColor?: str
                     border: `1px solid ${theme.palette.error.light}`,
                     color: theme.palette.error.main
                 };
+            case Tags.DISCOVERY:
+            case Tags.TALK:
             case Tags.VIDEO:
                 return {
                     background: 'background.default',
                     border: '1px solid #E0C2FF',
                     color: '#9C87B2'
                 };
+            case Tags.BEGINNER:
+            case Tags.QUICKY:
             case Tags.INTERVIEW:
                 return {
                     background: 'background.default',
                     border: `1px solid ${theme.palette.warning.main}`,
                     color: theme.palette.warning.main
+                };
+            case Tags.HUMAN_TECH:
+                return {
+                    background: 'background.default',
+                    border: `1px solid ${theme.palette.success.main}`,
+                    color: theme.palette.success.main
                 };
             case Tags.LIVE:
             case Tags.REPLAY:
@@ -49,6 +59,8 @@ export const ChipColor = (type: string, chipBackground?: string, chipColor?: str
                     border: '1px solid #F1931D',
                     color: '#F1931D'
                 };
+            case Tags.TECH:
+            case Tags.TECH_NOCODE:
             case Tags.FALLOUT:
                 return {
                     background: 'background.default',
