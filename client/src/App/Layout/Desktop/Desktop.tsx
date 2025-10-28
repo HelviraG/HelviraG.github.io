d16b249141dde979daa2a5f3e4b365eee1c29e7d
HelviraG <helvira.dev@gmail.com>
2025-10-28 01:02:00 +0100

    chore(app): add overall page slide transition

--- client/src/App/Layout/Desktop/Desktop.tsx
+++ client/src/App/Layout/Desktop/Desktop.tsx
@@ -1,18 +1,30 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, Ref, RefObject, useEffect, useRef, useState } from 'react';
 import { Header } from '@component/Header/Header';
 import { Footer } from '@component/Footer/Footer';
 import { ScrollTopButton } from '@component/ScrollTopButton/ScrollTopButton';
 import MouseContextProvider from '@context/MouseContextProvider';
 import { Cursor } from '@cursor';
+import { Box, Slide } from '@mui/material';
 
 export const Desktop = ({ children, withHeader = true, withTopButton = true, withFooter = true }: { children: ReactNode; withHeader?: boolean; withTopButton?: boolean; withFooter?: boolean }) => {
+    const containerRef = useRef<HTMLElement>(null);
+    const [mounted, setMounted] = useState<boolean>(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     return (
         <MouseContextProvider>
             <Cursor />
-            {withHeader && <Header />}
-            {children}
-            {withTopButton && <ScrollTopButton />}
-            {withFooter && <Footer />}
+            <Slide direction="left" in={mounted} mountOnEnter unmountOnExit container={containerRef.current}>
+                <Box ref={containerRef} component="main" sx={{ overflowX: 'hidden' }}>
+                    {withHeader && <Header />}
+                    {children}
+                    {withTopButton && <ScrollTopButton />}
+                    {withFooter && <Footer />}
+                </Box>
+            </Slide>
         </MouseContextProvider>
     )
 };
