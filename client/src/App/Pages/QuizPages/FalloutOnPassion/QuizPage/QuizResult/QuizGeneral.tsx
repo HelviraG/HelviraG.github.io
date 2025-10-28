3bb5872789783014e93710f71f0e87883c77d95a
HelviraG <helvira.dev@gmail.com>
2025-10-28 02:59:28 +0100

    chore(app): update translations for quiz fallout result page

--- client/src/App/Pages/QuizPages/FalloutOnPassion/QuizPage/QuizResult/QuizGeneral.tsx
+++ client/src/App/Pages/QuizPages/FalloutOnPassion/QuizPage/QuizResult/QuizGeneral.tsx
@@ -51,10 +51,10 @@ export const QuizGeneral = () => {
         <DashboardRightWrapperBox>
           <DashboardRightBox>
             <Divider textAlign={"right"} variant={"fullWidth"}>
-              <Chip label={'All the results'} />
+              <Chip label={t('app.explore.fallout_on_passion.quiz.result.all')} />
             </Divider>
             <Box sx={{ marginTop: 2 }}>
-              <Typography variant="h3" sx={{ fontWeight: 300 }}>Pick up your lane!</Typography>
+              <Typography variant="h3" sx={{ fontWeight: 300 }}>{t('app.explore.fallout_on_passion.quiz.result.title')}</Typography>
             </Box>
             <DashboardButtonsWrapper>
               {['coding', 'gaming', 'design', 'data'].map((activity: string) => (
