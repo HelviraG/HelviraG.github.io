122a95ccb4f248e87c0a72cf052d5f5fc9b420c5
HelviraG <helvira.dev@gmail.com>
2025-10-28 04:57:13 +0100

    chore(app): use common namespace to scroll text translation

--- client/src/Hooks/useScrollText.ts
+++ client/src/Hooks/useScrollText.ts
@@ -3,7 +3,7 @@ import { Routes } from "@resources/Enums/Routes";
 import { useTranslation } from "react-i18next";
 
 export default function useScrollText() {
-    const { t } = useTranslation();
+    const { t } = useTranslation('common');
     const pathname = useLocation().pathname;
 
     const getScrollText = () => {
@@ -11,55 +11,55 @@ export default function useScrollText() {
             case Routes.HOME:
                 return [
                     {
-                        "first_section": t('app.scroll.home.first_section'),
-                        "second_section": t('app.scroll.home.second_section'),
-                        "third_section": t('app.scroll.home.third_section'),
-                        "last_section": t('app.scroll.home.last_section')
+                        "first_section": t('scroll.home.first_section'),
+                        "second_section": t('scroll.home.second_section'),
+                        "third_section": t('scroll.home.third_section'),
+                        "last_section": t('scroll.home.last_section')
                     }
                 ];
             case Routes.CONFS:
                 return [
                     {
-                        "first_section": t('app.scroll.confs.first_section'),
-                        "second_section": t('app.scroll.confs.second_section'),
-                        "third_section": t('app.scroll.confs.third_section'),
-                        "last_section": t('app.scroll.confs.last_section')
+                        "first_section": t('scroll.confs.first_section'),
+                        "second_section": t('scroll.confs.second_section'),
+                        "third_section": t('scroll.confs.third_section'),
+                        "last_section": t('scroll.confs.last_section')
                     }
                 ];
             case Routes.LIVE:
                 return [
                     {
-                        "first_section": t('app.scroll.lives.first_section'),
-                        "second_section": t('app.scroll.lives.second_section'),
-                        "third_section": t('app.scroll.lives.third_section'),
-                        "last_section": t('app.scroll.lives.last_section')
+                        "first_section": t('scroll.lives.first_section'),
+                        "second_section": t('scroll.lives.second_section'),
+                        "third_section": t('scroll.lives.third_section'),
+                        "last_section": t('scroll.lives.last_section')
                     }
                 ];
             case Routes.PRESS:
                 return [
                     {
-                        "first_section": t('app.scroll.press.first_section'),
-                        "second_section": t('app.scroll.press.second_section'),
-                        "third_section": t('app.scroll.press.third_section'),
-                        "last_section": t('app.scroll.press.last_section')
+                        "first_section": t('scroll.press.first_section'),
+                        "second_section": t('scroll.press.second_section'),
+                        "third_section": t('scroll.press.third_section'),
+                        "last_section": t('scroll.press.last_section')
                     }
                 ];
             case Routes.CAREER:
                 return [
                     {
-                        "first_section": t('app.scroll.career.first_section'),
-                        "second_section": t('app.scroll.career.second_section'),
-                        "third_section": t('app.scroll.career.third_section'),
-                        "last_section": t('app.scroll.career.last_section')
+                        "first_section": t('scroll.career.first_section'),
+                        "second_section": t('scroll.career.second_section'),
+                        "third_section": t('scroll.career.third_section'),
+                        "last_section": t('scroll.career.last_section')
                     }
                 ];
             case Routes.EXPLORE:
                 return [
                     {
-                        "first_section": t('app.scroll.explore.first_section'),
-                        "second_section": t('app.scroll.explore.second_section'),
-                        "third_section": t('app.scroll.explore.third_section'),
-                        "last_section": t('app.scroll.explore.last_section')
+                        "first_section": t('scroll.explore.first_section'),
+                        "second_section": t('scroll.explore.second_section'),
+                        "third_section": t('scroll.explore.third_section'),
+                        "last_section": t('scroll.explore.last_section')
                     }
                 ];
             case Routes.PASSION:
@@ -68,10 +68,10 @@ export default function useScrollText() {
             case Routes.QUIZ_STEP:
                 return [
                     {
-                        "first_section": t('app.scroll.passion.first_section'),
-                        "second_section": t('app.scroll.passion.second_section'),
-                        "third_section": t('app.scroll.passion.third_section'),
-                        "last_section": t('app.scroll.passion.last_section')
+                        "first_section": t('scroll.passion.first_section'),
+                        "second_section": t('scroll.passion.second_section'),
+                        "third_section": t('scroll.passion.third_section'),
+                        "last_section": t('scroll.passion.last_section')
                     }
                 ];
             default:
