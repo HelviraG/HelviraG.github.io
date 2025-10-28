18ac55b48ab843bb5c6245876d13c793712a3316
HelviraG <helvira.dev@gmail.com>
2025-10-28 04:54:39 +0100

    chore(translation): add common namespace

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -2,12 +2,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import fr from './Translations/fr/fr.json';
 import en from './Translations/en/en.json';
+import commonEN from './Translations/en/common.json';
+import commonFR from './Translations/fr/common.json';
 
 const resources = {
   en: {
+    common: commonEN,
     translation: en
   },
   fr: {
+    common: commonFR,
     translation: fr
   }
 };
@@ -15,6 +19,8 @@ const resources = {
 i18n
   .use(initReactI18next)
   .init({
+    ns: ["translation", "common"],
+    defaultNS: "translation",
     resources,
     lng: "en",
     fallbackLng: "en",
