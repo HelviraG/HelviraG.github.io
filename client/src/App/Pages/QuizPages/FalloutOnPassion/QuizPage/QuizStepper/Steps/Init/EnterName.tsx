f0d6034d64dd955e0a24ca56e2eba4574a95ee1e
HelviraG <helvira.dev@gmail.com>
2025-10-28 05:05:40 +0100

    chore(fallout-passion): use translation namespace to EnterName

--- client/src/App/Pages/QuizPages/FalloutOnPassion/QuizPage/QuizStepper/Steps/Init/EnterName.tsx
+++ client/src/App/Pages/QuizPages/FalloutOnPassion/QuizPage/QuizStepper/Steps/Init/EnterName.tsx
@@ -23,7 +23,7 @@ export const EnterName = ({
   hasError,
   setHasError,
 }: EnterNameProps) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('translation');
 
   return (
     <>
