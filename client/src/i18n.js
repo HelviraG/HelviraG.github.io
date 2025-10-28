1c6acedef85ebb0078d72dd76435fb54f7fdd193
HelviraG <helvira.dev@gmail.com>
2025-10-28 07:41:06 +0100

    chore(app): update i18n init

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -4,14 +4,26 @@ import fr from './Translations/fr/fr.json';
 import en from './Translations/en/en.json';
 import commonEN from './Translations/en/common.json';
 import commonFR from './Translations/fr/common.json';
+import burnoutEN from './Translations/en/quiz/burnout_quiz.json';
+import burnoutFR from './Translations/fr/quiz/burnout_quiz.json';
+import falloutEN from './Translations/en/quiz/fallout_quiz.json';
+import falloutFR from './Translations/fr/quiz/fallout_quiz.json';
+import skillsEN from './Translations/en/quiz/skills_quiz.json';
+import skillsFR from './Translations/fr/quiz/skills_quiz.json';
 
 const resources = {
   en: {
     common: commonEN,
+    burnoutQuiz: burnoutEN,
+    falloutQuiz: falloutEN,
+    techSkillsQuiz: skillsEN,
     translation: en
   },
   fr: {
     common: commonFR,
+    burnoutQuiz: burnoutFR,
+    falloutQuiz: falloutFR,
+    techSkillsQuiz: skillsFR,
     translation: fr
   }
 };
@@ -19,7 +31,7 @@ const resources = {
 i18n
   .use(initReactI18next)
   .init({
-    ns: ["translation", "common"],
+    ns: ["translation", "common", "burnoutQuiz", "falloutQuiz", "skillsQuiz"],
     defaultNS: "translation",
     resources,
     lng: "en",
