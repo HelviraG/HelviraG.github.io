a1cd4e06e3daacee597a110b260c7253c5207655
HelviraG <helvira.dev@gmail.com>
2025-10-31 16:11:56 +0100

    chore(app): remove useless import to FalloutPassionPage

--- client/src/App/Pages/FalloutPassion.tsx
+++ client/src/App/Pages/FalloutPassion.tsx
@@ -4,7 +4,6 @@ import useDocumentTitle from "@hooks/useDocumentTitle";
 import { Desktop } from "../Layout/Desktop/Desktop";
 import { Mobile } from "../Layout/Mobile/Mobile";
 import { FalloutQuiz } from "./QuizPages/FalloutOnPassion/FalloutQuiz";
-import { BuyCoffeeLink } from "../Components/Link/BuyCoffeeLink";
 
 export const FalloutPassionPage = ({ isTablet }: { isTablet: boolean }) => {
   const { t } = useTranslation();
