253fd4f2079bffd3dc05c1a6982fd712b604d324
HelviraG <helvira.dev@gmail.com>
2025-11-05 02:02:54 +0100

    refactor(menu): update sidebar Menu

--- client/src/App/Components/Header/Menu/Menu.tsx
+++ client/src/App/Components/Header/Menu/Menu.tsx
@@ -1,21 +1,12 @@
-import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
-import FiberNewOutlinedIcon from '@mui/icons-material/FiberNewOutlined';
-import FilterCenterFocusRoundedIcon from '@mui/icons-material/FilterCenterFocusRounded';
-import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
-import NewspaperRoundedIcon from '@mui/icons-material/NewspaperRounded';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
-import WavingHandRoundedIcon from '@mui/icons-material/WavingHandRounded';
-import ImportContactsIcon from '@mui/icons-material/ImportContacts';
-import { Box, DrawerProps } from '@mui/material';
+import { alpha, Box, DrawerProps, IconButton } from '@mui/material';
 import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
-import useClickOutside from '@hooks/useClickOutside';
 import { Routes } from '@resources/Enums/Routes';
 import { Types } from '@resources/Enums/Types';
 import { RouterLink } from '@styles/Components/RouterLink';
-import { MenuIconBox, MenuIconButton, MenuItem, MenuItemText, MenuList, MenuListIcon, MenuListItemButton, MenuWrapper } from '@styles/Layout/Menu';
+import { MenuItem, MenuItemText, MenuList, MenuListIcon, MenuListItemButton, MenuWrapper } from '@styles/Layout/Menu';
+import { ArrowForward, BookmarkTwoTone, CodeTwoTone, FilterCenterFocusTwoTone, ImportContactsTwoTone, NewspaperTwoTone, PsychologyTwoTone, SportsEsportsTwoTone, VolumeUpTwoTone, WavingHandTwoTone } from '@mui/icons-material';
 
 interface MenuProps extends DrawerProps
 {
@@ -29,16 +20,27 @@ export const Menu = ({ openMenu, menuWidth, closeDrawer }: MenuProps) => {
     const pathname = useLocation().pathname;
 
     const drawerRef = useRef(null);
-
-    useClickOutside(drawerRef, closeDrawer);
  
     return (
-        <MenuWrapper anchor={Types.RIGHT} open={openMenu} menuWidth={menuWidth} PaperProps={{ ref: drawerRef }} menuColor={'#1E1E40'} ModalProps={{ keepMounted: true, disableScrollLock: true }}>
-            <MenuIconBox>
-                <MenuIconButton onClick={closeDrawer}>
-                    <HighlightOffRoundedIcon sx={{ color: 'background.default' }} />
-                </MenuIconButton>
-            </MenuIconBox>
+        <MenuWrapper variant={'persistent'} anchor={Types.RIGHT} open={openMenu} menuWidth={menuWidth} PaperProps={{ ref: drawerRef }} menuColor={'#6d74db'} ModalProps={{ keepMounted: true, disableScrollLock: true }}>
+            <Box 
+                sx={(theme) => ({ 
+                    alignItems: 'center',
+                    borderBottom: `.5px solid ${alpha('#6D74DB', 0.3)}`, 
+                    display: 'flex',
+                    height: '81px',
+                    padding: '0 16px',
+
+                    [theme.breakpoints.down(1200)]: {
+                        padding: '16px',
+                        borderBottom: `.5px solid ${alpha('#dddeef', 0.3)}`
+                    }
+                })}
+            >
+                <IconButton onClick={closeDrawer}>
+                    <ArrowForward sx={(theme) => ({ color: theme.game.special.dark })} />
+                </IconButton>
+            </Box>
             <Box>
                 <MenuList>
                     <MenuItem isFirstItem key={t('menu.universe')} >
@@ -46,42 +48,46 @@ export const Menu = ({ openMenu, menuWidth, closeDrawer }: MenuProps) => {
                     </MenuItem>
                     <MenuItem key={t('menu.home')}>
                         <MenuListItemButton component={RouterLink} {...{ to: Routes.HOME, isActive: pathname === Routes.HOME }}>
-                            <MenuListIcon><WavingHandRoundedIcon /></MenuListIcon>
+                            <MenuListIcon><WavingHandTwoTone /></MenuListIcon>
                             <MenuItemText primary={t('menu.home')} />
                         </MenuListItemButton>
                     </MenuItem>
                     <MenuItem key={t('menu.conferences')}>
                         <MenuListItemButton component={RouterLink} {...{ to: Routes.CONFS, isActive: pathname === Routes.CONFS }}>
-                            <MenuListIcon><VolumeUpRoundedIcon /></MenuListIcon>
+                            <MenuListIcon><VolumeUpTwoTone /></MenuListIcon>
                             <MenuItemText primary={t('menu.conferences')} />
+                        </MenuListItemButton>
+                    </MenuItem>
+                    <MenuItem key={t('menu.abstract')}>
+                        <MenuListItemButton component={RouterLink} {...{ to: Routes.ABSTRACT, isActive: pathname === Routes.ABSTRACT }}>
+                            <MenuListIcon><BookmarkTwoTone /></MenuListIcon>
+                            <MenuItemText primary={t('menu.abstract')} />
                         </MenuListItemButton>
                     </MenuItem>
                     <MenuItem key={t('menu.live')}>
                         <MenuListItemButton component={RouterLink} {...{ to: Routes.LIVE, isActive: pathname === Routes.LIVE }}>
-                            <MenuListIcon><FilterCenterFocusRoundedIcon /></MenuListIcon>
+                            <MenuListIcon><FilterCenterFocusTwoTone /></MenuListIcon>
                             <MenuItemText primary={t('menu.live')} />
                         </MenuListItemButton>
                     </MenuItem>
                     <MenuItem key={t('menu.explorer')}>
                         <MenuListItemButton component={RouterLink} {...{ to: Routes.EXPLORE, isActive: pathname === Routes.EXPLORE }}>
-                            <MenuListIcon><SportsEsportsIcon /></MenuListIcon>
+                            <MenuListIcon><SportsEsportsTwoTone /></MenuListIcon>
                             <MenuItemText primary={t('menu.explorer')} />
-                            <MenuListIcon iconColor='#FF6EC7'><FiberNewOutlinedIcon fontSize='large' /></MenuListIcon>
                         </MenuListItemButton>
                     </MenuItem>
-
                     <MenuItem isFirstItem withMargin key={t('menu.more')} >
                         <MenuItemText isFirstItem primary={t('menu.more')} />
                     </MenuItem>
                     <MenuItem key={t('menu.press')}>
                         <MenuListItemButton component={RouterLink} {...{ to: Routes.PRESS, isActive: pathname === Routes.PRESS }}>
-                            <MenuListIcon><ImportContactsIcon /></MenuListIcon>
+                            <MenuListIcon><ImportContactsTwoTone /></MenuListIcon>
                             <MenuItemText primary={t('menu.press')} />
                         </MenuListItemButton>
                     </MenuItem>
                     <MenuItem key={t('menu.career')}>
                         <MenuListItemButton component={RouterLink} {...{ to: Routes.CAREER, isActive: pathname === Routes.CAREER }}>
-                            <MenuListIcon><CodeRoundedIcon /></MenuListIcon>
+                            <MenuListIcon><CodeTwoTone /></MenuListIcon>
                             <MenuItemText primary={t('menu.career')} />
                         </MenuListItemButton>
                     </MenuItem>
